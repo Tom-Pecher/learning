5ba3453c55c1c1d85ae69d0f9ecc25d3d8814f20
main <tompecher.official@gmail.com>
2024-04-30 09:19:16 +0100

    Python W3Schools NumPy Section 11

---
+++ Python/W3Schools/NumPy_Tutorial/Basics/11_ArrayJoin.py
@@ -0,0 +1,38 @@
+
+# PYTHON: W3Schools - NumPy Tutorial
+# Section 11: Array Join
+
+# BASICS
+    # In numpy, we cannot use + to join ndarrays (this would add the contents), instead we can use np.concatenate():
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.concatenate((arr1, arr2))
+print("1.", arr)
+
+    # We can change the axis on which we join the ndarrays:
+arr1 = np.array([[1, 2], [3, 4]])
+arr2 = np.array([[5, 6], [7, 8]])
+arr = np.concatenate((arr1, arr2), axis=1)
+print("2.", arr)
+
+
+# STACKING
+    # When stacking two arrays, we essentially put one on top of the other (displayed as to the right):
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.stack((arr1, arr2), axis=1)
+print("3.", arr)
+
+    # The function np.hstack() stacks along rows:
+arr = np.hstack((arr1, arr2))
+print("4.", arr)
+
+    # The function np.vstack() stacks along columns:
+arr = np.vstack((arr1, arr2))
+print("5.", arr)
+
+    # The function np.dstack() stacks along height (depth):
+arr = np.dstack((arr1, arr2))
+print("6.", arr) 
