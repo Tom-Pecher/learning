d5c10c00da3baa8ec2a086cf037597a2d4588128
main <tompecher.official@gmail.com>
2024-05-07 07:04:19 +0100

    Python W3Schools NumPy Section 15

---
+++ Python/W3Schools/NumPy_Tutorial/Basics/15_ArrayFilter.py
@@ -0,0 +1,23 @@
+
+# PYTHON: W3Schools - NumPy Tutorial
+# Section 15: Array Filter
+
+# BASICS
+    # Using a list of booleans, we can filter the values of an ndarray:
+import numpy as np
+
+arr = np.array([41, 42, 43, 44])
+x = [True, False, True, False]
+print("1.", arr[x]) 
+
+arr = np.array([41, 42, 43, 44])
+
+    # Here is how we might iteratively generate a filter array:
+filter_arr = []
+for element in arr:
+    filter_arr.append(element > 42)
+print("2.", arr[filter_arr])
+
+    # The better way to do this is to apply the boolean to the whole ndarray and it will evaluate each element:
+filter_arr = arr % 2 == 0
+print("3.", arr[filter_arr])
