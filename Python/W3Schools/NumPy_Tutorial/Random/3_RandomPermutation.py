bee6152fef1e30c0a30c70f7f4ebd68c68c8c793
main <tompecher.official@gmail.com>
2024-05-10 13:04:00 +0100

    Python W3Schools NumPy Random Section 3

---
+++ Python/W3Schools/NumPy_Tutorial/Random/3_RandomPermutation.py
@@ -0,0 +1,16 @@
+
+# PYTHON: W3Schools - NumPy Tutorial
+# Section 3: Random Permutation
+
+from numpy import random
+import numpy as np
+
+# BASICS
+    # There are two functions for rearranging ndarray elements. First, random.shuffle() which shuffles the specified array:
+arr = np.array([1, 2, 3, 4, 5])
+random.shuffle(arr)
+print("1.", arr)
+
+    # And random.permutation() which does the same thing except it returns a copy instead:
+arr = np.array([1, 2, 3, 4, 5])
+print("2.", random.permutation(arr)) 
