e831e0f933eebe03736a9c7d42ed8116b385836d
main <tompecher.official@gmail.com>
2024-05-11 10:30:33 +0100

    Python W3Schools NumPy Random Section 4

---
+++ Python/W3Schools/NumPy_Tutorial/Random/4_SeabornModule.py
@@ -0,0 +1,11 @@
+
+# PYTHON: W3Schools - NumPy Tutorial
+# Section 4: Seaborn Module
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# BASICS
+    # The seaborn module is a module that allows us to plot probability distributions using mathplotlib
+sns.histplot([0, 1, 2, 3, 4, 5])
+plt.show() 
