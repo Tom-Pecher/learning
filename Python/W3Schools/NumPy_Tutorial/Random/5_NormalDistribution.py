b5a07b7f3064c6c5ae02d065bddfa4ccb7dd4647
main <tompecher.official@gmail.com>
2024-05-12 11:32:04 +0100

    Python W3Schools NumPy Random Section 5

---
+++ Python/W3Schools/NumPy_Tutorial/Random/5_NormalDistribution.py
@@ -0,0 +1,18 @@
+
+# PYTHON: W3Schools - NumPy Tutorial
+# Section 5: Normal Distribution
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from numpy import random
+
+# BASICS
+    # The function random.normal() will create an ndarray of a specified size where the elements are sampled from the standard normal distribution:
+print("1.", random.normal(size=(2, 3)))
+
+    # We can specify a mean and standard deviation with the loc and scale keyword arguments respectively:
+print("2.", random.normal(loc=1, scale=2, size=(2, 3)))
+
+    # As mentioned previously, we can use the seaborn module to visualize this distribution:
+sns.histplot(random.normal(size=1000))
+plt.show()
