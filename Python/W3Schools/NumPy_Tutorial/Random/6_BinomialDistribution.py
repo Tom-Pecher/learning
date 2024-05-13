caccdf5b26e734b493a6f13461a213e704af0084
main <tompecher.official@gmail.com>
2024-05-13 09:00:13 +0100

    Python W3Schools NumPy Random Section 6

---
+++ Python/W3Schools/NumPy_Tutorial/Random/6_BinomialDistribution.py
@@ -0,0 +1,15 @@
+
+# PYTHON: W3Schools - NumPy Tutorial
+# Section 6: Binomial Distribution
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from numpy import random
+
+# BASICS
+    # The function random.binomial() sample from a binomial distribution with the specified n and p:
+print("1.", random.binomial(n=10, p=0.5, size=10))
+
+    # Again, we can plot the distribution using the seaborn module:
+sns.histplot(random.binomial(n=10, p=0.5, size=1000), kde=False)
+plt.show()
