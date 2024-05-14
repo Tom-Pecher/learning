f6894d9382531e9cd6a358e813aae8c12f343ddf
main <tompecher.official@gmail.com>
2024-05-14 12:30:25 +0100

    Python W3Schools NumPy Random Section 8

---
+++ Python/W3Schools/NumPy_Tutorial/Random/8_UniformDistribution.py
@@ -0,0 +1,15 @@
+
+# PYTHON: W3Schools - NumPy Tutorial
+# Section 8: Uniform Distribution
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from numpy import random
+
+# BASICS
+    # The function random.uniform() samples from a continuous uniform distribution with the specified upper and lower bounds (0 and 1 by default):
+print("1.", random.uniform(size=(2, 3)))
+
+    # Again, we can plot the distribution using the seaborn module:
+sns.histplot(random.uniform(size=1000), kde=False)
+plt.show()
