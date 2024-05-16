9761f5a4b63fdc2b899d3ba80a0dea106caaa027
main <tompecher.official@gmail.com>
2024-05-16 11:01:47 +0100

    Python W3Schools NumPy Random Section 9

---
+++ Python/W3Schools/NumPy_Tutorial/Random/9_LogisticDistribution.py
@@ -0,0 +1,15 @@
+
+# PYTHON: W3Schools - NumPy Tutorial
+# Section 9: Logistic Distribution
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from numpy import random
+
+# BASICS
+    # The function random.logistic() samples from a logistic distribution with the specified mean (loc) and standard deviation (scale):
+print("1.", random.logistic(loc=1, scale=2, size=(2, 3)))
+
+    # Again, we can plot the distribution using the seaborn module:
+sns.histplot(random.logistic(size=1000), kde=False)
+plt.show()
