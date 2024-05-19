166892bfb0e4c9f7b6b2f4d6023e25a6240b8cff
main <tompecher.official@gmail.com>
2024-05-19 20:59:54 +0100

    Python W3Schools NumPy Random Section 11

---
+++ Python/W3Schools/NumPy_Tutorial/Random/11_ExponentialDistribution.py
@@ -0,0 +1,15 @@
+
+# PYTHON: W3Schools - NumPy Tutorial
+# Section 11: Exponential Distribution
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from numpy import random
+
+# BASICS
+    # The function random.exponential() samples from an exponential distribution with the specified inverse lambda value:
+print("1.", random.exponential(scale=2, size=(2, 3)))
+
+    # Again, we can plot the distribution using the seaborn module:
+sns.histplot(random.exponential(size=1000), kde=False)
+plt.show()
