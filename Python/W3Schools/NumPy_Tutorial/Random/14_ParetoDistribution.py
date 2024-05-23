8ecd12c11c13bee2ab5aeee92c8a80de66592e6a
main <tompecher.official@gmail.com>
2024-05-23 09:29:50 +0100

    Python W3Schools NumPy Random Section 14

---
+++ Python/W3Schools/NumPy_Tutorial/Random/14_ParetoDistribution.py
@@ -0,0 +1,15 @@
+
+# PYTHON: W3Schools - NumPy Tutorial
+# Section 14: Pareto Distribution
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from numpy import random
+
+# BASICS
+    # The function random.pareto() samples from a Pareto distribution with the specified shape parameter:
+print("1.", random.pareto(a=2, size=(2, 3)))
+
+    # Again, we can plot the distribution using the seaborn module:
+sns.histplot(random.pareto(a=2, size=1000), kde=False)
+plt.show()
