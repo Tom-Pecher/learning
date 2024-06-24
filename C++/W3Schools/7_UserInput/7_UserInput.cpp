1c3aa49c9fe737d2f4288d3459f10f5b44dcf847
main <tompecher.official@gmail.com>
2024-06-24 19:26:02 +0100

    C++ W3Schools Section 7

---
+++ C++/W3Schools/7_UserInput/7_UserInput.cpp
@@ -0,0 +1,16 @@
+
+// C++: W3Schools - Main Tutorial
+// Section 7: User Input
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // USER INPUT
+  int x; 
+  cout << "Type a number: ";
+  cin >> x;                                   // Get user input from the keyboard
+  cout << "Your number is: " << x;            // Display user input.
+  return 0;
+}
