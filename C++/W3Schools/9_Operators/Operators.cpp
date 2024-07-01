8aed9a8735d0de25baa6ae6010dac3cb74de93cd
main <tompecher.official@gmail.com>
2024-07-01 12:32:34 +0100

    C++ W3Schools Section 9

---
+++ C++/W3Schools/9_Operators/Operators.cpp
@@ -0,0 +1,47 @@
+
+// C++: W3Schools - Main Tutorial
+// Section 8: Data Types
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+  // ARITHMETIC
+      //    + 	  Addition 	        Adds together two values 	                x + y
+      //    - 	  Subtraction 	    Subtracts one value from another 	        x - y
+      //    * 	  Multiplication 	  Multiplies two values 	                  x * y
+      //    / 	  Division 	        Divides one value by another 	            x / y
+      //    % 	  Modulus 	        Returns the division remainder 	          x % y
+      //    ++ 	  Increment 	      Increases the value of a variable by 1 	  ++x 	
+      //    -- 	  Decrement 	      Decreases the value of a variable by 1 	  --x
+
+  // ASSIGNMENT
+      //    = 	        x = 5 	        x = 5 	
+      //    += 	        x += 3 	        x = x + 3 	
+      //    -= 	        x -= 3 	        x = x - 3 	
+      //    *= 	        x *= 3 	        x = x * 3 	
+      //    /= 	        x /= 3 	        x = x / 3 	
+      //    %= 	        x %= 3 	        x = x % 3 	
+      //    &= 	        x &= 3 	        x = x & 3 	
+      //    |= 	        x |= 3 	        x = x | 3 	
+      //    ^= 	        x ^= 3 	        x = x ^ 3 	
+      //    >>= 	      x >>= 3 	      x = x >> 3 	
+      //    <<= 	      x <<= 3 	      x = x << 3
+
+  // COMPARISON
+      //    == 	        Equal to 	                  x == y 	
+      //    != 	        Not equal 	                x != y 	
+      //    > 	        Greater than 	              x > y 	
+      //    < 	        Less than 	                x < y 	
+      //    >= 	        Greater than or equal to 	  x >= y 	
+      //    <= 	        Less than or equal to 	    x <= y
+
+  // LOGICAL
+      //    &&  	  Logical and 	    Returns true if both statements are true 	x < 5 &&  x < 10 	
+      //    ||  	  Logical or 	      Returns true if one of the statements is true 	x < 5 || x < 4 	
+      //    ! 	    Logical not 	    Reverse the result, returns false if the result is true 	!(x < 5 && x < 10)
+
+  return 0;
+}
