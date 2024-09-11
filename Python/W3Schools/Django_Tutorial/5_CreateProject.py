6e7bc4650a14ec49fe454da36a30e57588876575
main <tompecher.official@gmail.com>
2024-09-11 17:28:03 +0100

    Python W3Schools Django Section 5

---
+++ Python/W3Schools/Django_Tutorial/5_CreateProject.py
@@ -0,0 +1,10 @@
+
+# PYTHON: W3Schools - Django Tutorial
+# Section 5: Create Project
+
+# COMMANDS
+    # Create Project
+# django-admin startproject my_tennis_club
+
+    # Navigate to the /my_tennis_club
+# py manage.py runserver
