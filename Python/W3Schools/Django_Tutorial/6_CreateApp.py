583b0f11d135998a2c561e39c095e6500e3ea0b2
main <tompecher.official@gmail.com>
2024-09-12 23:50:04 +0100

    Python W3Schools Django Section 6

---
+++ Python/W3Schools/Django_Tutorial/6_CreateApp.py
@@ -0,0 +1,9 @@
+
+# PYTHON: W3Schools - Django Tutorial
+# Section 6: Create App
+
+# An app is a web application that has a specific meaning in your project, like a home page, a contact form, or a members database.
+
+# COMMANDS
+    # Create App
+# py manage.py startapp members
