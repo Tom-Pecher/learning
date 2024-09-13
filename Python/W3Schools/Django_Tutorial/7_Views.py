d8837318703491add15cbb34a4c1921bee1f7e8b
main <tompecher.official@gmail.com>
2024-09-13 15:42:17 +0100

    Python W3Schools Django Section 7

---
+++ Python/W3Schools/Django_Tutorial/7_Views.py
@@ -0,0 +1,13 @@
+
+# PYTHON: W3Schools - Django Tutorial
+# Section 7: Views
+
+# Django views are Python functions that takes http requests and returns http response, like HTML documents.
+
+# CHANGES:
+    # In my_tennis_club/members/views.py
+# from django.shortcuts import render
+# from django.http import HttpResponse
+
+# def members(request):
+#     return HttpResponse("Hello world!")
