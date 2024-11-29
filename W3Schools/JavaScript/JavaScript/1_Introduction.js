919c947196f2c3f2c4ca7fa89c6bdab9d0bcd844
main <tompecher.official@gmail.com>
2024-11-29 16:50:29 +0000

    JavaScript W3Schools Section 1

---
+++ W3Schools/JavaScript/JavaScript/1_Introduction.js
@@ -0,0 +1,9 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 1: Introduction
+
+// JavaScript can:
+//   - Change HTML content
+//   - Change HTML attributes
+//   - Change HTML styles (CSS)
+//   - Hide/Show HTML elements
