0f51c8f0f12fb02a5b02b608a233978cd5bc9144
main <tompecher.official@gmail.com>
2024-11-29 17:25:43 +0000

    JS W3Schools Section 4

---
+++ W3Schools/JavaScript/JavaScript/4_Statements.js
@@ -0,0 +1,32 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 4: Statements
+
+// In JavaScript, statements are separated by semicolons:
+let a, b, c;
+a = 5;
+b = 6;
+
+// This is optional but it is a good practice to use it:
+c = a + b
+
+// Whitespace is ignored in JavaScript:
+let person1 = "Hege";
+let person2="Hege";
+
+// JavaScript statements can be grouped together in code blocks, inside curly brackets {...}:
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello Dolly!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+}
+
+// Important JavaScript keywords:
+//  var 	    Declares a variable
+//  let 	    Declares a block variable
+//  const 	    Declares a block constant
+//  if 	        Marks a block of statements to be executed on a condition
+//  switch 	    Marks a block of statements to be executed in different cases
+//  for 	    Marks a block of statements to be executed in a loop
+//  function 	Declares a function
+//  return 	    Exits a function
+//  try 	    Implements error handling to a block of statements
