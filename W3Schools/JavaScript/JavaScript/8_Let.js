b5a464c3062af8eaa14f22427d5204e27a724fa1
main <tompecher.official@gmail.com>
2024-11-30 03:10:32 +0000

    JS W3Schools Section 8

---
+++ W3Schools/JavaScript/JavaScript/8_Let.js
@@ -0,0 +1,13 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 8: Let
+
+// Variables declared with let have block scope (cannot be accessed outside the block):
+{
+    let x = 2;
+}
+
+//          Scope	Redeclare	Reassign	Hoisted	    Binds this
+// var	    No      Yes	        Yes	        Yes	        Yes
+// let	    Yes     No	        Yes	        No	        No
+// const	Yes     No	        No	        No	        No
