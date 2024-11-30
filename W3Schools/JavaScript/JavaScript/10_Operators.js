a46d68293e5e5b05c42e48ef2a961327541fd57b
main <tompecher.official@gmail.com>
2024-11-30 03:22:59 +0000

    JS W3Schools Section 10

---
+++ W3Schools/JavaScript/JavaScript/10_Operators.js
@@ -0,0 +1,57 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 10: Operators
+
+
+// Arithmetic operators
+//  + 	    Addition
+//  - 	    Subtraction
+//  * 	    Multiplication
+//  ** 	    Exponentiation
+//  / 	    Division
+//  % 	    Modulus (Division Remainder)
+//  ++ 	    Increment
+//  -- 	    Decrement
+
+// Assignment operators
+//  x = y 	    x = y
+//  x += y 	    x = x + y
+//  x -= y 	    x = x - y
+//  x *= y 	    x = x * y
+//  x /= y 	    x = x / y
+//  x %= y 	    x = x % y
+//  x **= y 	x = x ** y
+
+// Comparison operators
+//  == 	    equal to
+//  === 	equal value and equal type
+//  != 	    not equal
+//  !== 	not equal value or not equal type
+//  > 	    greater than
+//  < 	    less than
+//  >= 	    greater than or equal to
+//  <= 	    less than or equal to
+//  ? 	    ternary operator
+
+// Comparison operators also work on strings:
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+
+// Logical operators
+//  && 	    logical and
+//  || 	    logical or
+//  ! 	    logical not
+
+// Type operators
+// typeof 	    Returns the type of a variable
+// instanceof 	Returns true if an object is an instance of an object type
+
+// Bitwise operators
+//  & 	    AND
+//  | 	    OR
+//  ~ 	    NOT
+//  ^ 	    XOR
+//  << 	    left shift
+//  >> 	    right shift
+//  >>> 	unsigned right shift
