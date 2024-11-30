e4a40e1100f1886f8d7cf2f4ae534e132663a2c9
main <tompecher.official@gmail.com>
2024-11-30 02:38:12 +0000

    JS W3Schools Section 5

---
+++ W3Schools/JavaScript/JavaScript/5_Syntax.js
@@ -0,0 +1,29 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 5: Syntax
+
+// The JavaScript syntax defines two types of values:
+//     Fixed values:        literals
+//     Variable values:     variables
+
+// Literals
+//  Numbers:
+10.50
+1001
+
+//  Strings:
+"John Doe"
+'John Doe'
+
+// Variables
+let x;  // Declares a variable x (declare with let, var or const)
+x = 6;  // Assigns the value 6 to x
+
+// Operators:
+//    We use operators to form expressions:
+(5 + 6) * 10
+
+// Comments:
+//    Single line comment
+/* Multi-line
+   comment */
