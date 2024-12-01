92acda6c0f28038ea45ea6e892d4f38f0e802ee7
main <tompecher.official@gmail.com>
2024-12-01 17:42:21 +0000

    JS W3Schools Section 13

---
+++ W3Schools/JavaScript/JavaScript/13_DataTypes.js
@@ -0,0 +1,22 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 13: Data Types
+
+
+// JavaScript Data Types:
+//  - String
+//  - Number
+//  - Bigint
+//  - Boolean
+//  - Undefined
+//  - Null
+//  - Symbol
+//  - Object 
+
+// The object data type can contain both built-in objects, and user defined objects.
+// Built-in object types can be: objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more. 
+
+// JavaScript data types are dynamic:
+let x;       // Now x is undefined
+x = 5;       // Now x is a Number
+x = "John";  // Now x is a String
