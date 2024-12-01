cc54f9c7b3c1b90f195264388b95d39760e98ad9
main <tompecher.official@gmail.com>
2024-12-01 17:35:09 +0000

    JS W3Schools Section 11

---
+++ W3Schools/JavaScript/JavaScript/11_Arithmetic.js
@@ -0,0 +1,14 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 11: Arithmetic
+
+
+// Arithmetic operators:
+//  + 	Addition
+//  - 	Subtraction
+//  * 	Multiplication
+//  ** 	Exponentiation (ES2016)
+//  / 	Division
+//  % 	Modulus (Remainder)
+//  ++ 	Increment
+//  -- 	Decrement
