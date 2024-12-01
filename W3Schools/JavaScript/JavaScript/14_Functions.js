759bf6a7d7187fd8c804936f912120fbff6d7214
main <tompecher.official@gmail.com>
2024-12-01 17:46:34 +0000

    JS W3Schools Section 14

---
+++ W3Schools/JavaScript/JavaScript/14_Functions.js
@@ -0,0 +1,10 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 14: Functions
+
+
+// JavaScript functions use the function keyword:
+function myFunction(p1, p2) {
+    // The return statement returns a value back to the caller:
+    return p1 * p2;
+}
