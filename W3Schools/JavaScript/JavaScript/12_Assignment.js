cb6150636607da4f0e369ed2199b42fbced9326a
main <tompecher.official@gmail.com>
2024-12-01 17:38:10 +0000

    JS W3Schools Section 12

---
+++ W3Schools/JavaScript/JavaScript/12_Assignment.js
@@ -0,0 +1,22 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 12: Assignment
+
+
+// Assignment operators:
+//  = 	    x = y
+//  += 	    x += y
+//  -= 	    x -= y
+//  *= 	    x *= y
+//  /= 	    x /= y
+//  %= 	    x %= y
+//  **= 	x **= y
+//  <<= 	x <<= y
+//  >>= 	x >>= y
+//  >>>= 	x >>>= y
+//  &= 	    x &= y
+//  ^= 	    x ^= y
+//  |= 	    x |= y
+//  &&= 	x &&= y
+//  ||= 	x ||= y
+//  ??= 	x ??= y
