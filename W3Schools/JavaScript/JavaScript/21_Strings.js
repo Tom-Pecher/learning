180f34be7d5129a81d1e5b60bd249781a14c0c82
main <tompecher.official@gmail.com>
2024-12-04 09:30:48 +0000

    JS W3Schools Section 21

---
+++ W3Schools/JavaScript/JavaScript/21_Strings.js
@@ -0,0 +1,24 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 21: Strings
+
+// A string is a collection of characters:
+let carName1 = "Volvo XC60";  // Double quotes
+let carName2 = 'Volvo XC60';  // Single quotes 
+
+// Quotes can be nested so long as they are different:
+let answer = "He is called 'Johnny'";
+
+// Alternatively, one can used a template string (backticks):
+let text = `He's often called "Johnny"`
+
+// To obtain the length of a string, we can use the length property:
+let length = text.length;
+
+// There are 6 main escape characters in JavaScript:
+//  \b 	Backspace
+//  \f 	Form Feed
+//  \n 	New Line
+//  \r 	Carriage Return
+//  \t 	Horizontal Tabulator
+//  \v 	Vertical Tabulator
