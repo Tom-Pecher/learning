6790c898f1a9375eeb920948d3823c863ae180d7
main <tompecher.official@gmail.com>
2024-12-04 09:51:43 +0000

    JS W3Schools Section 24

---
+++ W3Schools/JavaScript/JavaScript/24_StringTemplates.js
@@ -0,0 +1,18 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 24: String Templates
+
+// String templates use backticks instead of quotes:
+let text1 = `He's often called "Johnny"`;
+
+// These allow multiline strings and the use of quotes within strings:
+let text2 =
+`The quick
+brown fox
+jumps over
+the lazy dog`;
+
+// We can also substitute variables into string templates:
+let firstName = "John";
+let lastName = "Doe";
+let text3 = `Welcome ${firstName}, ${lastName}!`;
