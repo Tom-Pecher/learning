c0513eb63eff95fb080b9aca94b226cd8b386cd4
main <tompecher.official@gmail.com>
2024-12-05 04:06:48 +0000

    JS W3Schools Section 25

---
+++ W3Schools/JavaScript/JavaScript/25_Numbers.js
@@ -0,0 +1,26 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 25: Numbers
+
+
+// JavaScript has only one type of number. Numbers can be written with or without decimals.
+let a = 3.14;
+let b = 3;
+
+// Using scientific notation:
+let x = 123e5;
+let y = 123e-5;
+
+// JavaScript will return NaN for numbers that are impossible:
+let test = 100 / "Apple";
+isNaN(test);
+typeof NaN;
+
+// JavaScript will return Infinity for numbers that are too large:
+while (myNumber != Infinity) {
+    myNumber = myNumber * myNumber;
+}
+let inf =  2 / 0;
+
+// Hexadecimal numbers are written with a leading 0x:
+let hex = 0xFF;
