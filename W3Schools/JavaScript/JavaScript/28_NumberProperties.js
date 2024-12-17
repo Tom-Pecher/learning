c70dce5e7b584f9df82546db1774888581cd9731
main <tompecher.official@gmail.com>
2024-12-17 08:55:19 +0000

    JS W3Schools Section 28

---
+++ W3Schools/JavaScript/JavaScript/28_NumberProperties.js
@@ -0,0 +1,17 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 28: Number Properties
+
+
+// Number Properties:
+// EPSILON 	            The difference between 1 and the smallest number > 1.
+// MAX_VALUE 	        The largest number possible in JavaScript
+// MIN_VALUE 	        The smallest number possible in JavaScript
+// MAX_SAFE_INTEGER 	The maximum safe integer (253 - 1)
+// MIN_SAFE_INTEGER 	The minimum safe integer -(253 - 1)
+// POSITIVE_INFINITY 	Infinity (returned on overflow)
+// NEGATIVE_INFINITY 	Negative infinity (returned on overflow)
+// NaN 	                A "Not-a-Number" value
+
+// Example:
+let x = Number.MAX_SAFE_INTEGER;
