43f2e70e453c3f410fa59b779492523a38c25ad5
main <tompecher.official@gmail.com>
2024-12-29 21:40:59 +0000

    JS W3Schools Section 37

---
+++ W3Schools/JavaScript/JavaScript/37_DateGetMethods.js
@@ -0,0 +1,28 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 37: Date Get Methods
+
+// Here is a list of methods for extracting temporal data:
+    // getFullYear() 	    Get year as a four digit number (yyyy)
+    // getMonth() 	        Get month as a number (0-11)
+    // getDate() 	        Get day as a number (1-31)
+    // getDay() 	        Get weekday as a number (0-6)
+    // getHours() 	        Get hour (0-23)
+    // getMinutes() 	    Get minute (0-59)
+    // getSeconds() 	    Get second (0-59)
+    // getMilliseconds() 	Get millisecond (0-999)
+    // getTime() 	        Get time (milliseconds since January 1, 1970)
+
+// Date.now() returns the number of milliseconds since January 1, 1970:
+Date.now();
+
+// UTC Date Get Methods:
+    // getUTCDate() 	        Returns the UTC date
+    // getUTCFullYear()         Returns the UTC year
+    // getUTCMonth()            Returns the UTC month
+    // getUTCDay()              Returns the UTC day
+    // getUTCHours()            Returns the UTC hour
+    // getUTCMinutes()          Returns the UTC minutes
+    // getUTCSeconds()          Returns the UTC seconds
+    // getUTCMilliseconds() 	Returns the UTC milliseconds
+    
