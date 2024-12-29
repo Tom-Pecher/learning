bb8ca2142e5ebb1b7b722c78dab53e4c376da0e5
main <tompecher.official@gmail.com>
2024-12-29 21:28:37 +0000

    JS W3Schools Section 34

---
+++ W3Schools/JavaScript/JavaScript/34_ArrayConst.js
@@ -0,0 +1,12 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 34: Array Const
+
+// An array declared with const cannot be reassigned:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// It does not define a constant array. It defines a constant reference to an array:
+cars[0] = "Toyota";
+
+// An array declared with const must be initialized when it is declared.
+// An array with const has block scope.
