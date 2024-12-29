839b16ab1506c8e0f758614092bca26b1dd7417b
main <tompecher.official@gmail.com>
2024-12-29 21:37:52 +0000

    JS W3Schools Section 36

---
+++ W3Schools/JavaScript/JavaScript/36_DateFormats.js
@@ -0,0 +1,11 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 36: Date Formats
+
+// JavaScript recognises 3 date formats:
+    // ISO Date 	"2015-03-25" (The International Standard)
+    // Short Date 	"03/25/2015"
+    // Long Date 	"Mar 25 2015" or "25 Mar 2015"
+
+// The Date.parse() method can take a date string and return the number of milliseconds since January 1, 1970, 00:00:00 UTC:
+Date.parse("March 21, 2012");
