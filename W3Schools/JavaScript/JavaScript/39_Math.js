e49fc1e2e3e67ef16699506529f974a96feefa21
main <tompecher.official@gmail.com>
2025-01-04 19:47:17 +0000

    JS W3Schools Section 39

---
+++ W3Schools/JavaScript/JavaScript/39_Math.js
@@ -0,0 +1,71 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 39: Math
+
+var d = new Date();
+
+// The JavaScript Math object allows you to perform mathematical tasks on numbers.
+
+// Math properities (constants):
+Math.E        // returns Euler's number
+Math.PI       // returns PI
+Math.SQRT2    // returns the square root of 2
+Math.SQRT1_2  // returns the square root of 1/2
+Math.LN2      // returns the natural logarithm of 2
+Math.LN10     // returns the natural logarithm of 10
+Math.LOG2E    // returns base 2 logarithm of E
+Math.LOG10E   // returns base 10 logarithm of E 
+
+
+// Math methods (number to integer):
+    // Math.round(x) returns the value of x rounded to its nearest integer:
+Math.round(4.7)
+
+    // Math.ceil(x) returns the value of x rounded up to its nearest integer:
+Math.ceil(4.4)
+
+    // Math.floor(x) returns the value of x rounded down to its nearest integer:
+Math.floor(4.7)
+
+    // Math.trunc(x) returns the integer part of x:
+Math.trunc(4.7)
+
+
+    // Math.sign(x) returns -1, 0 or 1 if x is negative, zero or positive:
+Math.sign(-4.7)
+
+
+// Math methods (general):
+    // Math.pow(x, y) returns the value of x to the power of y:
+Math.pow(8, 2)
+
+    // Math.sqrt(x) returns the square root of x:
+Math.sqrt(64)
+
+    // Math.abs(x) returns the absolute (positive) value of x:
+Math.abs(-4.7)
+
+    // Math.sin(x) returns the sine of x (given in radians):
+Math.sin(0)
+
+    // Math.cos(x) returns the cosine of x (given in radians):
+Math.cos(0)
+
+    // Math.min() and Math.max() can be used to find the lowest or highest value in a list of arguments:
+Math.min(0, 150, 30, 20, -8, -200)
+Math.max(0, 150, 30, 20, -8, -200)
+
+
+    // Math.random() returns a random number between 0 (inclusive), and 1 (exclusive):
+Math.random()
+
+
+// Math methods (logarithms):
+    // Math.log(x) returns the natural logarithm (base E) of x:
+Math.log(10)
+
+    // Math.log2(x) returns the base 2 logarithm of x:
+Math.log2(10)
+
+    // Math.log10(x) returns the base 10 logarithm of x:
+Math.log10(10)
