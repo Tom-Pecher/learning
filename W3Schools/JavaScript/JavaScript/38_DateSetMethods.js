ddd0608c05bc61fb297e0ae0eb2ad38234edbd6d
main <tompecher.official@gmail.com>
2025-01-04 19:38:19 +0000

    JS W3Schools Section 38

---
+++ W3Schools/JavaScript/JavaScript/38_DateSetMethods.js
@@ -0,0 +1,27 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 38: Date Set Methods
+
+var d = new Date();
+
+// The setFullYear() method sets the year of a date object:
+d.setFullYear(2020);
+
+// The setMonth() method sets the month of a date object (0-11):
+d.setMonth(11);
+
+// The setDate() method sets the day of a date object (1-31):
+d.setDate(15);
+
+// The setHours() method sets the hour of a date object (0-23):
+d.setHours(22);
+
+// The setMinutes() method sets the minutes of a date object (0-59):
+d.setMinutes(30);
+
+// The setSeconds() method sets the seconds of a date object (0-59):
+d.setSeconds(30);
+
+// Dates can be compared like any other variable:
+d > new Date();
+    
