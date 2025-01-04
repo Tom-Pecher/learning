15c3c23f864789dd1ba6174eec10e034cb0779d2
main <tompecher.official@gmail.com>
2025-01-04 20:05:54 +0000

    JS W3Schools Section 43

---
+++ W3Schools/JavaScript/JavaScript/43_IfElse.js
@@ -0,0 +1,13 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 43: If Else
+
+
+// If Else Statements:
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
