11a4e4081286f02d2d0154d9266ce0ac8d9d849a
main <tompecher.official@gmail.com>
2025-01-04 20:10:02 +0000

    JS W3Schools Section 44

---
+++ W3Schools/JavaScript/JavaScript/44_Switch.js
@@ -0,0 +1,23 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 43: Switch
+
+
+// Switch Statements:
+switch (new Date().getDay()) {
+  case 1:
+    day = "Monday";
+    break;
+  case 2:   // Reuse code for case 2 and 3
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  default:  // default case
+    day = "Weekend";
+}
