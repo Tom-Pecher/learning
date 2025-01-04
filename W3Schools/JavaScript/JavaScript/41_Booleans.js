52bdca89e4129fc89abeff1350bd05800509a676
main <tompecher.official@gmail.com>
2025-01-04 19:59:46 +0000

    JS W3Schools Section 41

---
+++ W3Schools/JavaScript/JavaScript/41_Booleans.js
@@ -0,0 +1,37 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 41: Booleans
+
+// We can evaluate expressions to determine if they are true or false:
+Boolean(10 > 9)
+(10 > 9)
+10 > 9
+
+// Everything with a value is true:
+Boolean(100)
+Boolean(3.14)
+Boolean(-15)
+Boolean("Hello")
+Boolean("false")
+Boolean(7 + 1 + 3.14)
+
+// Everything without a value is false:
+Boolean(0)
+Boolean(-0)
+Boolean("")
+Boolean(null)
+
+// Boolean Operators:
+// AND  &&
+// OR   ||
+// NOT  !
+
+// Comparison Operators:
+// ==   equal
+// ===  strictly equal
+// !=   not equal
+// !==  strictly not equal
+// >    greater than
+// <    less than
+// >=   greater than or equal to
+// <=   less than or equal to
