30f44e3fa716e311b0e7e81959c67e18d8c6376a
main <tompecher.official@gmail.com>
2025-01-05 20:46:53 +0000

    JS W3Schools Section 51

---
+++ W3Schools/JavaScript/JavaScript/51_Sets.js
@@ -0,0 +1,11 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 51: Sets
+
+
+// Sets allow you to create collections of unique values:
+const letters = new Set(["a","b","c"]);
+
+// Add a new value to the set:
+letters.add("a");
+letters.add("d");
