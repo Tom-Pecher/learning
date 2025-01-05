a01a1d8edf3c534222db422cd5049448b0507195
main <tompecher.official@gmail.com>
2025-01-05 20:34:38 +0000

    JS W3Schools Section 47

--- W3Schools/JavaScript/JavaScript/46_LoopForIn.js
+++ W3Schools/JavaScript/JavaScript/46_LoopForIn.js
@@ -3,7 +3,7 @@
 // Section 46: For Loop In
 
 
-// We can loop across all of the elements in a collection:
+// We can loop across all of the properties in an object:
 const person = {fname:"John", lname:"Doe", age:25};
 
 let text = "";
---
+++ W3Schools/JavaScript/JavaScript/47_LoopForOf.js
@@ -0,0 +1,20 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 47: For Loop Of
+
+
+// We can loop across all of the elements in a collection:
+const cars = ["BMW", "Volvo", "Mini"];
+
+let text1 = "";
+for (let x of cars) {
+  text1 += x;
+}
+
+// This can be done for strings too:
+let language = "JavaScript";
+
+let text2 = "";
+for (let x of language) {
+text2 += x;
+}
