b570d560c22ab58be096fa2a47e6dfb0b83502b2
main <tompecher.official@gmail.com>
2025-01-05 20:32:42 +0000

    JS W3Schools Section 46

---
+++ W3Schools/JavaScript/JavaScript/46_LoopForIn.js
@@ -0,0 +1,12 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 46: For Loop In
+
+
+// We can loop across all of the elements in a collection:
+const person = {fname:"John", lname:"Doe", age:25};
+
+let text = "";
+for (let x in person) {
+  text += person[x];
+}
