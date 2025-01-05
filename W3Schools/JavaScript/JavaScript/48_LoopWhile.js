1ec8b431f2411bf68bc03fb8ee38c39505d3f8a3
main <tompecher.official@gmail.com>
2025-01-05 20:36:07 +0000

    JS W3Schools Section 48

---
+++ W3Schools/JavaScript/JavaScript/48_LoopWhile.js
@@ -0,0 +1,17 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 48: Loop While
+
+
+// The syntax for a while loop is as follows:
+while (i < 10) {
+  text += "The number is " + i;
+  i++;
+}
+
+// Likewise, syntax for a do-while loop is as follows:
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10);
