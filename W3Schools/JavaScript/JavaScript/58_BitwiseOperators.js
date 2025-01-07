03d54aac8155f09610172ee97410b3f476febb7d
main <tompecher.official@gmail.com>
2025-01-07 23:35:23 +0000

    JS W3Schools Section 58

---
+++ W3Schools/JavaScript/JavaScript/58_BitwiseOperators.js
@@ -0,0 +1,12 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 58: Bitwise Operators
+
+// JavaScript Bitwise Operators:
+//  & 	    AND 	                Sets each bit to 1 if both bits are 1
+//  | 	    OR 	                    Sets each bit to 1 if one of two bits is 1
+//  ^ 	    XOR 	                Sets each bit to 1 if only one of two bits is 1
+//  ~ 	    NOT 	                Inverts all the bits
+//  << 	    Zero fill left shift 	Shifts left by pushing zeros in from the right and let the leftmost bits fall off
+//  >> 	    Signed right shift 	    Shifts right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+//  >>> 	Zero fill right shift 	Shifts right by pushing zeros in from the left, and let the rightmost bits fall off
