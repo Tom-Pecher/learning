47dba119e258502be31cf2bc75dc0aba4aed44c8
main <tompecher.official@gmail.com>
2025-01-09 21:44:01 +0000

    JS W3Schools Section 59

---
+++ W3Schools/JavaScript/JavaScript/59_Precedence.js
@@ -0,0 +1,12 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 59: Precedence
+
+// JavaScript Operator Precedence:
+// ( ) 	    Expression Grouping
+// . 	    Member Of
+// [] 	    Member Of
+// ?. 	    Optional Chaining
+// () 	    Function Call
+// new 	    With arguments
+// new      No arguments
