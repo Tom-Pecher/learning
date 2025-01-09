b4845d2b337a7359bb8d5754cc5c9945bdcc9ba1
main <tompecher.official@gmail.com>
2025-01-09 23:20:16 +0000

    JS W3Schools Section 65

---
+++ W3Schools/JavaScript/JavaScript/65_ArrowFunction.js
@@ -0,0 +1,9 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 65: Arrow Function
+
+// Arrow functions are designed to be more concise and easier to read than traditional function expressions:
+let myFunction = (a, b) => a * b;
+
+// Arrow functions return the value of the expression by default:
+hello = () => "Hello World!"; 
