2081056440c6a226408c3eb1d0ffd7fa1a6a1ba1
main <tompecher.official@gmail.com>
2025-01-09 21:57:55 +0000

    JS W3Schools Sections 61

---
+++ W3Schools/JavaScript/JavaScript/61_Scope.js
@@ -0,0 +1,8 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 61: Scope
+
+// As discussed previously, there are 3 types of scope in JavaScript:
+    // Block scope
+    // Function scope
+    // Global scope
