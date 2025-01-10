3975e61cc4eefe30bca1daeb1b56d5ddeae21940
main <tompecher.official@gmail.com>
2025-01-10 22:31:38 +0000

    JS W3Schools Section 72

---
+++ W3Schools/JavaScript/JavaScript/72_Mistakes.js
@@ -0,0 +1,15 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 72: Mistakes
+
+// Common Mistakes:
+    // Accidentally using the assignment operator (=) instead of the equality operator.
+    // Using the equality operator == in a switch statement (it expects strict, ===).
+    // Confusing addition and concatenation.
+    // Misunderstanding floats and precision.
+    // Breaking strings over multiple lines.
+    // Misplacing semicolons.
+    // Accessing arrays with named indexes.
+    // Ending expressions with trailing commas.
+    // Confusing null and undefined.
+    
