2453f523d3b3184f2123f6a3e754d5671fd0edb2
main <tompecher.official@gmail.com>
2025-01-10 22:13:35 +0000

    JS W3Schools Section 69

---
+++ W3Schools/JavaScript/JavaScript/69_Debugging.js
@@ -0,0 +1,12 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 69: Debugging
+
+// All modern browsers have a built-in JavaScript debugger.
+
+// Log variables to make them appear in the debug console:
+var c = 0;
+console.log(c)
+
+// Use the debugger keyword to stop the execution of JavaScript, and call (or open) the debugging function:
+debugger;
