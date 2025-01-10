de9ef974de1cb55b9f414598d65b481ccd57101f
main <tompecher.official@gmail.com>
2025-01-10 22:23:08 +0000

    JS W3Schools Section 71

---
+++ W3Schools/JavaScript/JavaScript/71_BestPractices.js
@@ -0,0 +1,21 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 71: Best Practices
+
+// Avoid global variables
+// Always declare local variables
+// Put all declarations at the top of each script or function
+// Initialize all variables when you declare them
+// Declare object and arrays with const
+// Avoid automatic type conversions
+// Use === and !== instead of == and !=
+// Set default values for parameters
+// End switch cases with default
+
+// Use ""               instead of new String()
+// Use 0                instead of new Number()
+// Use false            instead of new Boolean()
+// Use {}               instead of new Object()
+// Use []               instead of new Array()
+// Use /()/             instead of new RegExp()
+// Use function (){}    instead of new Function()
