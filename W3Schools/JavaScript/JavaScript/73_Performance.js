1a65f668b36818b84d6de41637585faa3981b33b
main <tompecher.official@gmail.com>
2025-01-10 22:46:46 +0000

    JS W3Schools Section 73

---
+++ W3Schools/JavaScript/JavaScript/73_Performance.js
@@ -0,0 +1,12 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 73: Performance
+
+// For better performace:
+    // Reduce activity in loops
+    // Reduce DOM access
+    // Reduce DOM size
+    // Avoid unnecessary variables
+    // Avoid unnecessary functions
+    // Delay javaScript loading
+    // Avoid using the with statement
