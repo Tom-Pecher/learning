a7ab66a4acae862a78f03b2f039e7e70162574da
main <tompecher.official@gmail.com>
2025-01-10 22:18:04 +0000

    JS W3Schools Section 70

---
+++ W3Schools/JavaScript/JavaScript/70_StyleGuide.js
@@ -0,0 +1,23 @@
+
+// JAVASCRIPT: W3Schools - Main Tutorial
+// Section 70: Style Guide
+
+// VARIABLES
+// Variable names should be descriptive and not too long.
+// Use camelCase for variable names.
+// Use UPPERCASE for constant values.
+// Use a space before and after operators.
+// Indentation should be 2 spaces.
+
+// STATEMENTS
+// Always end a simple statement with a semicolon.
+// Put the opening bracket at the end of the first line.
+// Use one space before the opening bracket.
+// Use one space before the closing bracket.
+// Do not end a complex statement with a semicolon.
+
+// OBJECTS
+// Use object literal notation {} for object creation.
+
+
+// For readability, avoid lines longer than 80 characters.
