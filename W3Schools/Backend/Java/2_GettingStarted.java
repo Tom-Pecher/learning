68a8da0f33038ed98b88fc99d9b5fc0f190e6c25
main <tompecher.official@gmail.com>
2025-01-17 22:39:27 +0000

    Java W3Schools Section 2

---
+++ W3Schools/Backend/Java/2_GettingStarted.java
@@ -0,0 +1,9 @@
+
+// Java: W3Schools - Main Tutorial
+// Section 2: Getting Started
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
