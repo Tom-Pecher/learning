b8b60f6cdb17571d56663613f5f49e5745f2a19d
main <tompecher.official@gmail.com>
2025-01-17 22:41:45 +0000

    Java W3Schools Section 3

---
+++ W3Schools/Backend/Java/3_Syntax.java
@@ -0,0 +1,12 @@
+
+// Java: W3Schools - Main Tutorial
+// Section 3: Syntax
+
+// Every line of code that runs in Java must be inside a class:
+public class Main {
+    // The main() method is required and you will see it in every Java program:
+    public static void main(String[] args) {
+        // We can use the println() method to print a line of text to the screen:
+        System.out.println("Hello World");
+    }
+}
