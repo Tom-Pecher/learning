aacb5d9ea044b6e9d54eeb012c5656abab0ccc73
main <tompecher.official@gmail.com>
2025-01-17 22:44:27 +0000

    Java W3Schools Section 4

---
+++ W3Schools/Backend/Java/4_Output.java
@@ -0,0 +1,24 @@
+
+// Java: W3Schools - Main Tutorial
+// Section 4: Output
+
+public class Main {
+  public static void main(String[] args) {
+
+    // PRINT TEXT
+    // We can use the println() method to print a line of text to the screen:
+    System.out.println("Hello World!");
+
+    // The print() method does not create a new line:
+    System.out.print("I will print on the same line.");
+
+
+    // PRINT NUMBERS
+    // We can also print numbers:
+    System.out.println(50000);
+
+    // As well as calculations:
+    System.out.println(3 + 3);
+    
+  }
+}
