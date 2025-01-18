ab5be271598b338d4ab24f84416fac26d2988fb9
main <tompecher.official@gmail.com>
2025-01-18 16:23:48 +0000

    Java W3Schools Section 7

---
+++ W3Schools/Backend/Java/7_DataTypes.java
@@ -0,0 +1,14 @@
+
+// Java: W3Schools - Main Tutorial
+// Section 7: Data Types
+
+// Java has 8 primitive data types, they are:
+    // byte 	  Stores whole numbers from -128 to 127
+    // short 	  Stores whole numbers from -32,768 to 32,767
+    // int 	    Stores whole numbers from -2,147,483,648 to 2,147,483,647
+    // long 	  Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+    // float 	  Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+    // double 	Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits
+    // boolean 	Stores true or false values
+    // char 	  Stores a single character/letter or ASCII values
+    
