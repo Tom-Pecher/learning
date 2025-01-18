7bb2135e380d82f88fcd904a820afa3960de23db
main <tompecher.official@gmail.com>
2025-01-18 16:39:55 +0000

    Java W3Schools Section 9

---
+++ W3Schools/Backend/Java/9_Operators.java
@@ -0,0 +1,40 @@
+
+// Java: W3Schools - Main Tutorial
+// Section 9: Operators
+
+
+// Arithmetic Operators
+  //    + 	  Addition 	        Adds together two values 	                x + y 	
+  //    - 	  Subtraction 	    Subtracts one value from another 	        x - y 	
+  //    * 	  Multiplication 	  Multiplies two values 	                  x * y 	
+  //    / 	  Division 	        Divides one value by another 	            x / y 	
+  //    % 	  Modulus 	        Returns the division remainder 	          x % y 	
+  //    ++ 	  Increment 	      Increases the value of a variable by 1 	  ++x 	
+  //    -- 	  Decrement 	      Decreases the value of a variable by 1 	  --x
+
+// Assignment Operators
+  //    = 	    x = 5 	    x = 5 	
+  //    += 	    x += 3 	    x = x + 3 	
+  //    -= 	    x -= 3 	    x = x - 3 	
+  //    *= 	    x *= 3 	    x = x * 3 	
+  //    /= 	    x /= 3 	    x = x / 3 	
+  //    %= 	    x %= 3 	    x = x % 3 	
+  //    &= 	    x &= 3 	    x = x & 3 	
+  //    |= 	    x |= 3 	    x = x | 3 	
+  //    ^= 	    x ^= 3 	    x = x ^ 3 	
+  //    >>= 	  x >>= 3 	  x = x >> 3 	
+  //    <<= 	  x <<= 3 	  x = x << 3
+
+// Comparison Operators
+  //    == 	    Equal to 	                  Returns true if the values are equal
+  //    != 	    Not equal 	                Returns true if the values are not equal
+  //    > 	    Greater than 	              Returns true if the left value is greater than the right value
+  //    < 	    Less than 	                Returns true if the left value is less than the right value	
+  //    >= 	    Greater than or equal to 	  Returns true if the left value is greater than or equal to the right value 	
+  //    <= 	    Less than or equal to 	    Returns true if the left value is less than or equal to the right value
+
+// Logical Operators
+  //    && 	    Logical and 	      Returns true if both statements are true
+  //    || 	    Logical or 	        Returns true if one of the statements is true
+  //    ! 	    Logical not 	      Reverse the result, returns false if the result is true
+  
