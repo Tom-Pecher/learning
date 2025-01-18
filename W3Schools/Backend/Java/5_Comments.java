5f1c675cf02432f6d346bc16d67b2a94b1234a7b
main <tompecher.official@gmail.com>
2025-01-18 16:16:49 +0000

    Java W3Schools Section 6

--- W3Schools/Backend/Java/5_Comments.java
+++ W3Schools/Backend/Java/5_Comments.java
@@ -3,5 +3,6 @@
 // Section 5: Comments
 
 // This is a single-line comment/
+
 /* This is a 
 multi-line comment */
---
+++ W3Schools/Backend/Java/6_Variables.java
@@ -0,0 +1,32 @@
+
+// Java: W3Schools - Main Tutorial
+// Section 6: Variables
+
+// Java Variable Types:
+//      - String
+//      - int
+//      - float
+//      - boolean
+//      - char
+
+public class Main {
+  public static void main(String[] args) {
+
+    int myNum1;    // Declare a variable
+    myNum1 = 15;   // Assign a value
+
+    // Print the value of the variable:
+    System.out.println(myNum1);
+
+    // Final variables cannot be changed once they are assigned a value:
+    final int myNum2 = 15;
+    
+    // Example of all variable types:
+    int myNum = 5;
+    float myFloatNum = 5.99f;
+    char myLetter = 'D';
+    boolean myBool = true;
+    String myText = "Hello";
+    
+  }
+}
