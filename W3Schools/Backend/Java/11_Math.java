204d0331545962eb683365f71ac690c7d8104d81
main <tompecher.official@gmail.com>
2025-01-19 11:09:52 +0000

    Java W3Schools Section 11

---
+++ W3Schools/Backend/Java/11_Math.java
@@ -0,0 +1,40 @@
+
+// Java: W3Schools - Main Tutorial
+// Section 11: Math
+
+
+public class Main {
+  public static void main(String[] args) {
+
+    // Find the maximum of two numbers:
+    System.out.println(Math.max(5, 10));
+
+    // Find the minimum of two numbers:
+    System.out.println(Math.min(5, 10));
+
+    // Find the square root of a number:
+    System.out.println(Math.sqrt(64));
+
+    // Find the absolute value of a number:
+    System.out.println(Math.abs(-4.7));
+
+    // Generate a random number between 0 and 1:
+    System.out.println(Math.random());
+
+    // Generate a random number between 0 and 100:
+    System.out.println(Math.random() * 100);
+
+    // Generate a random number between 0 and 100, then round it:
+    System.out.println((int)(Math.random() * 100));
+
+    // Round a number:
+    System.out.println(Math.round(4.7));
+
+    // Take the floor of a number:
+    System.out.println(Math.floor(4.7));
+
+    // Take the ceiling of a number:
+    System.out.println(Math.ceil(4.4));
+
+  }
+}
