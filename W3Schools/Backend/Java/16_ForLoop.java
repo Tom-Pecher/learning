eb7811063f5481db6341eca28ff2106ac643a796
main <tompecher.official@gmail.com>
2025-01-19 11:25:28 +0000

    Java W3Schools Section 16

---
+++ W3Schools/Backend/Java/16_ForLoop.java
@@ -0,0 +1,21 @@
+
+// Java: W3Schools - Main Tutorial
+// Section 16: For Loop
+
+
+public class Main {
+  public static void main(String[] args) {
+
+    // The for loop allows us to iterate over values of a variable:
+    for (int i = 0; i < 5; i++) {
+      System.out.println(i);
+    }
+
+    // Similarly, the for-each loop allows us to iterate over values of a collection:
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    for (String i : cars) {
+      System.out.println(i);
+    }
+
+  }
+}
