46547936681fecf2fa7669384e13bf502f594649
main <tompecher.official@gmail.com>
2025-01-19 11:20:38 +0000

    Java W3Schools Section 15

---
+++ W3Schools/Backend/Java/15_WhileLoop.java
@@ -0,0 +1,27 @@
+
+// Java: W3Schools - Main Tutorial
+// Section 15: While Loop
+
+
+public class Main {
+  public static void main(String[] args) {
+
+    int i = 0;
+
+    // We can use a while loop to execute code until a condition is met:
+    while (i < 5) {
+      System.out.println(i);
+      i++;
+    }
+
+    int j = 0;
+
+    // The do-while loop first runs the code before chekcing the condition:
+    do {
+      System.out.println(j);
+      j++;
+    }
+    while (j < 5);
+
+  }
+}
