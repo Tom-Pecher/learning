b39337c98f58c281cd2a618a4fd1a2f24a6dfb19
main <tompecher.official@gmail.com>
2025-01-19 11:11:49 +0000

    Java W3Schools Section 12

---
+++ W3Schools/Backend/Java/12_Booleans.java
@@ -0,0 +1,18 @@
+
+// Java: W3Schools - Main Tutorial
+// Section 12: Booleans
+
+
+public class Main {
+  public static void main(String[] args) {
+
+    // Booleans
+    boolean isJavaFun = true;
+
+    // Conditional statements return boolean values:
+    int x = 10;
+    int y = 9;
+    System.out.println(x > y);
+
+  }
+}
