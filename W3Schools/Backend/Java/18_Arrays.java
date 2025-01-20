5f1b1608281773e06bb6dc4e47d3ab381197edba
main <tompecher.official@gmail.com>
2025-01-20 17:40:14 +0000

    Java W3Schools Section 18

---
+++ W3Schools/Backend/Java/18_Arrays.java
@@ -0,0 +1,19 @@
+
+// Java: W3Schools - Main Tutorial
+// Section 18: Arrays
+
+
+public class Main {
+  public static void main(String[] args) {
+
+  // Java arrays are mutable, fixed-size data structures that hold elements of the same type:
+  String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+  // We can access and modify elements in an array using an index:
+  cars[0] = "Opel";
+
+  // We can also access the length of an array:
+  System.out.println(cars.length);
+  
+  }
+}
