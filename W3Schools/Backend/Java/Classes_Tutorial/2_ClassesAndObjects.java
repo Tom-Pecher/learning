4e0d9b19a42f06e2c27aa62a2449aff904f167bc
main <tompecher.official@gmail.com>
2025-01-22 21:06:23 +0000

    Java W3Schools Section 2

---
+++ W3Schools/Backend/Java/Classes_Tutorial/2_ClassesAndObjects.java
@@ -0,0 +1,15 @@
+
+// Java: W3Schools - Classes Tutorial
+// Section 2: Classes and Objects
+
+// Classes are blueprints for objects.
+public class Main {
+  int x = 5;
+
+  public static void main(String[] args) {
+    Main myObj1 = new Main();   // Objects are specific instances of a class.
+    Main myObj2 = new Main();
+    System.out.println(myObj1.x);
+    System.out.println(myObj2.x);
+  }
+}
