4a4afe6a0e3fa8541c89903b28a9185a2fcbd609
main <tompecher.official@gmail.com>
2025-01-22 21:03:45 +0000

    Java W3Schools Classes Section 1

---
+++ W3Schools/Backend/Java/Classes_Tutorial/1_OOP.java
@@ -0,0 +1,6 @@
+
+// Java: W3Schools - Classes Tutorial
+// Section 1: OOP
+
+
+// Java is based on Object-Oriented Programming (OOP), which is a programming paradigm that uses objects to represent data and methods to manipulate that data.
