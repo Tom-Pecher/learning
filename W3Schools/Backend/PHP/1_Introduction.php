595d59b021747c645a3682049b633944107d7882
main <tompecher.official@gmail.com>
2025-01-23 17:39:26 +0000

    PHP W3Schools Section 1

---
+++ W3Schools/Backend/PHP/1_Introduction.php
@@ -0,0 +1,10 @@
+
+<!-- SQL: W3Schools - Main Tutorial -->
+<!-- Section 1: Introduction -->
+
+<!-- 
+    PHP is an acronym for "PHP: Hypertext Preprocessor"
+    PHP is a widely-used, open source scripting language
+    PHP scripts are executed on the server
+    PHP is free to download and use
+ -->
