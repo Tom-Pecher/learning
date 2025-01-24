7180b5266b6fff2d649eeee1df78201141bbdca5
main <tompecher.official@gmail.com>
2025-01-24 16:35:24 +0000

    C W3Schools Section 4

---
+++ W3Schools/Backend/C/4_Output/4_Output.c
@@ -0,0 +1,16 @@
+
+// C: W3Schools - Main Tutorial
+// Section 4: Output
+
+ #include <stdio.h>
+
+int main() {
+  // Use the printf() function to output text:
+  printf("Hello World!");
+  printf("I am learning C.");
+  printf("And it is awesome!");
+
+  // The \n character is used to insert a new line:
+  printf("Hello World!\nI am learning C.\nAnd it is awesome!");
+  return 0;
+}
