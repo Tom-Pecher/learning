136e4ff95cbfb612f2bf9b0247553e92b986baeb
main <tompecher.official@gmail.com>
2025-01-24 16:38:34 +0000

    C W3Schools Section 5

---
+++ W3Schools/Backend/C/5_Comments/5_Comments.c
@@ -0,0 +1,12 @@
+
+// C: W3Schools - Main Tutorial
+// Section 5: Comments
+
+ #include <stdio.h>
+
+int main() {
+  // This is a single-line comment
+  /* This is a 
+  multi-line comment.*/
+  return 0;
+}
