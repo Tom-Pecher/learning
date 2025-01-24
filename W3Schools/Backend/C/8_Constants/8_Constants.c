76ad47cc041399cb8183ceb268eeaa837daaa3ab
main <tompecher.official@gmail.com>
2025-01-24 17:13:45 +0000

    C W3Schools Section 8

---
+++ W3Schools/Backend/C/8_Constants/8_Constants.c
@@ -0,0 +1,13 @@
+
+// C: W3Schools - Main Tutorial
+// Section 8: Constants
+
+ #include <stdio.h>
+
+int main() {
+  // Constants are like variables except that once they are defined, they cannot be changed or redefined.
+  const int BIRTHYEAR = 1980;
+
+  // It is a better convention to define constants in uppercase.
+  return 0;
+}
