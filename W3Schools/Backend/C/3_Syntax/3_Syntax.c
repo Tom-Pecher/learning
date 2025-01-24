344f4eaae9bad7af55ea78fe97a195b498a0838a
main <tompecher.official@gmail.com>
2025-01-24 16:29:34 +0000

    C W3Schools Section 3

---
+++ W3Schools/Backend/C/3_Syntax/3_Syntax.c
@@ -0,0 +1,10 @@
+
+// C: W3Schools - Main Tutorial
+// Section 3: Syntax
+
+#include <stdio.h>            // Include the stdio.h library, a header file in the C Standard Library for input and output.
+
+int main() {                  // Define the main function, the entry point of the program.
+  printf("Hello World!");     // Print the string "Hello World!" to the console.
+  return 0;                   // Return 0 to the operating system to indicate successful completion of the program.
+}
