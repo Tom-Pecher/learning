f8155c3f106f4c0eacad0fda3f1f93469d5a0943
main <tompecher.official@gmail.com>
2025-01-25 18:42:20 +0000

    C W3Schools Section 9

---
+++ W3Schools/Backend/C/9_Operators/9_Operators.c
@@ -0,0 +1,43 @@
+
+// C: W3Schools - Main Tutorial
+// Section 9: Operators
+
+int main() {
+  return 0;
+}
+
+// Arithmetic Operators
+    // + 	  Addition
+    // - 	  Subtraction
+    // * 	  Multiplication
+    // / 	  Division
+    // % 	  Modulus
+    // ++ 	Increment
+    // -- 	Decrement
+
+// Assignment Operators
+    // = 	  Assign
+    // += 	Add and assign
+    // -= 	Subtract and assign
+    // *= 	Multiply and assign
+    // /= 	Divide and assign
+    // %= 	Modulus and assign
+    // &= 	Bitwise AND and assign
+    // |= 	Bitwise OR and assign
+    // ^= 	Bitwise XOR and assign
+    // >>= 	Bitwise right shift and assign
+    // <<= 	Bitwise left shift and assign
+
+// Comparison Operators
+    // == 	Equal to
+    // != 	Not equal to
+    // > 	  Greater than
+    // < 	  Less than
+    // >= 	Greater than or equal to
+    // <= 	Less than or equal to
+
+// Logical Operators
+    // && 	Logical and
+    // || 	Logical or
+    // ! 	  Logical not
+    
