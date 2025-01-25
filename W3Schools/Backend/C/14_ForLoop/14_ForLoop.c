0afcec038811cd3a5654875bc459c8514740d7fb
main <tompecher.official@gmail.com>
2025-01-25 19:00:10 +0000

    C W3Schools Section 14

---
+++ W3Schools/Backend/C/14_ForLoop/14_ForLoop.c
@@ -0,0 +1,15 @@
+
+// C: W3Schools - Main Tutorial
+// Section 14: For Loop
+
+#include <stdio.h>
+
+int main() {
+
+    // The for loop allows us to iterate over a sequence of values:
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
