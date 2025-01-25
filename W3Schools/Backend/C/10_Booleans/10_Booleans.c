df1e60f89492214542dcbf50e3a34b638d917f81
main <tompecher.official@gmail.com>
2025-01-25 18:46:22 +0000

    C W3Schools Section 10

---
+++ W3Schools/Backend/C/10_Booleans/10_Booleans.c
@@ -0,0 +1,18 @@
+
+// C: W3Schools - Main Tutorial
+// Section 10: Booleans
+
+// Include the stdbool.h header file to use the bool data type.
+#include <stdbool.h> 
+#include <stdio.h>
+
+// The bool data type is used to store only two possible values: true or false.
+int main() {
+  bool isHamburgerTasty = true;     // true (1)
+  bool isPizzaTasty = false;        // false (0)
+
+  // Perform comnparisons on booleans:
+  printf("%d", isHamburgerTasty == isPizzaTasty);
+
+  return 0;
+}
