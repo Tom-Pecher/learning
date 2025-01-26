6b57b32704e55c38ada7ea345ab4e75bee5217ba
main <tompecher.official@gmail.com>
2025-01-26 23:27:46 +0000

    R W3Schools Section 3

---
+++ W3Schools/Backend/R/3_Syntax.R
@@ -0,0 +1,13 @@
+
+# R: W3Schools - Main Tutorial
+# Section 3: Syntax
+
+# To output text, use quotation marks:
+"Hello World!"
+
+# For numbers, no extra characters are needed:
+42
+
+# To output a variable, use the print() function:
+x <- 5
+print(x)
