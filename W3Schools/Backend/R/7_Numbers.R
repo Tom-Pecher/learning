cc80f9f2caa5295927e4e6a49cf6acd8e36dc208
main <tompecher.official@gmail.com>
2025-01-26 23:54:52 +0000

    R W3Schools Section 7

---
+++ W3Schools/Backend/R/7_Numbers.R
@@ -0,0 +1,13 @@
+
+# R: W3Schools - Main Tutorial
+# Section 7: Numbers
+
+# There are three types of numbers in R:
+x <- 10.5   # numeric
+y <- 10L    # integer
+z <- 1i     # complex 
+
+# We can convert between these types:
+a = as.integer(y)
+b = as.numeric(z)
+c = as.complex(x)
