6642af7b41b76069c1efc8d1e636c04b97dd7ade
main <tompecher.official@gmail.com>
2025-01-26 23:41:33 +0000

    R W3Schools Section 6

---
+++ W3Schools/Backend/R/6_DataTypes.R
@@ -0,0 +1,25 @@
+
+# R: W3Schools - Main Tutorial
+# Section 6: Data Types
+
+# There are 5 basic data types in R:
+
+# numeric
+x <- 10.5
+class(x)    # we can use the class() function to check the data type of a variable
+
+# integer
+x <- 1000L
+class(x)
+
+# complex
+x <- 9i + 3
+class(x)
+
+# character/string
+x <- "R is exciting"
+class(x)
+
+# logical/boolean
+x <- TRUE
+class(x)
