d727868bc2982d5379bf128fa1db88cebc19f3fd
main <tompecher.official@gmail.com>
2025-01-27 00:18:42 +0000

    R W3Schools Section 8

---
+++ W3Schools/Backend/R/8_Math.R
@@ -0,0 +1,20 @@
+
+# R: W3Schools - Main Tutorial
+# Section 8: Math
+
+# Operators
+    #   +	Addition
+    #   -	Subtraction
+    #   *	Multiplication
+    #   /	Division
+    #   ^	Exponentiation
+
+# Math Functions
+    # abs(x)	            Absolute value
+    # sqrt(x)	            Square root
+    # ceiling(x)	        Rounds up to the nearest integer
+    # floor(x)	            Rounds down to the nearest integer
+    # trunc(x)	            Truncates to the nearest integer
+    # round(x, digits=n)	Rounds to n digits
+    # signif(x, digits=n)	Rounds to n significant digits
+    
