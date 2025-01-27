e948e7ae8e462aac99824929dcf513e465b0b248
main <tompecher.official@gmail.com>
2025-01-27 00:35:05 +0000

    R W3Schools Section 10

---
+++ W3Schools/Backend/R/10_Booleans.R
@@ -0,0 +1,13 @@
+
+# R: W3Schools - Main Tutorial
+# Section 10: Booleans
+
+# Booleans are logical values that work the same way as in any language:
+a <- 200
+b <- 33
+
+if (b > a) {
+  print ("b is greater than a")
+} else {
+  print("b is not greater than a")
+} 
