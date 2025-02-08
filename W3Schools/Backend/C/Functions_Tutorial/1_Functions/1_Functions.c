d0a2d0ea001d8ebc7fe13c460a5ecf7b5afb8a7e
main <tompecher.official@gmail.com>
2025-02-08 21:51:13 +0000

    C W3Schools Functions Section 1

---
+++ W3Schools/Backend/C/Functions_Tutorial/1_Functions/1_Functions.c
@@ -0,0 +1,15 @@
+
+// C: W3Schools - Functions Tutorial
+// Section 1: Functions
+
+#include <stdio.h>
+
+// Functions are pieces of code that can be reused (as in most other languages).
+void myFunction() { // When defining our function, we must specify a return type and a name.
+  printf("I just got executed!");
+}
+
+int main() {
+  myFunction(); // Call the function
+  return 0;
+}
