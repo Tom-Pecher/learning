5fe985e94136e7d795e07120c8b47f4aa02f7f05
main <tompecher.official@gmail.com>
2025-02-09 15:37:31 +0000

    C W3Schools Section 2

---
+++ W3Schools/Backend/C/Other/2_Enums/2_Enums.c
@@ -0,0 +1,34 @@
+
+// C: W3Schools - Other Topics Tutorial
+// Section 2: Enums
+
+#include <stdio.h>
+#include <string.h>
+
+// An enum is a group of constants:
+enum Level1 {
+  LOW1,
+  MEDIUM1,
+  HIGH1
+};
+
+// We can assign values like so:
+enum Level2 {
+  LOW2 = 25,
+  MEDIUM2 = 50,
+  HIGH2 = 75
+};
+
+// If we set one value, the others will also change:
+enum Level3 {
+  LOW3 = 5,
+  MEDIUM3, // Now 6
+  HIGH3 // Now 7
+};
+
+int main() {
+  enum Level1 myVar = MEDIUM1;
+  printf("%d", myVar);
+
+  return 0;
+}
