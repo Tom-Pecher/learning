4e8b54390c88832ec3f786ce014c7fdc5a3bd9fa
main <tompecher.official@gmail.com>
2025-02-11 11:16:59 +0000

    R W3Schools Section 12

---
+++ W3Schools/Backend/R/12_IfElse.R
@@ -0,0 +1,15 @@
+
+# R: W3Schools - Main Tutorial
+# Section 12: If Else
+
+# We can write conditional statements like in any other language:
+a <- 200
+b <- 33
+
+if (b > a) {
+  print("b is greater than a")
+} else if (a == b) {
+  print("a and b are equal")
+} else {
+  print("a is greater than b")
+}
