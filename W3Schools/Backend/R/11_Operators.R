5928a84f3ea96160e9a48eeb55f7d89cfd961193
main <tompecher.official@gmail.com>
2025-02-11 11:14:38 +0000

    R W3Schools Section 11

---
+++ W3Schools/Backend/R/11_Operators.R
@@ -0,0 +1,43 @@
+
+# R: W3Schools - Main Tutorial
+# Section 11: Operators
+
+# ARITHMETIC OPERATORS
+  #   + 	  Addition	
+  #   - 	  Subtraction
+  #   * 	  Multiplication	
+  #   / 	  Division
+  #   ^ 	  Exponent
+  #   %% 	  Modulus (Remainder from division)	
+  #   %/% 	Integer Division
+
+
+# ASSIGNMENT OPERATORS
+  #   <- 	  Leftward assignment
+  #   -> 	  Rightward assignment
+  #   = 	  Assignment
+  #   <<- 	Global assignment
+  #   ->> 	Global rightward assignment
+
+
+# COMPARISON OPERATORS
+  #   == 	  Equal to
+  #   != 	  Not equal to
+  #   > 	  Greater than
+  #   < 	  Less than
+  #   >= 	  Greater than or equal to
+  #   <= 	  Less than or equal to
+
+
+# LOGICAL OPERATORS
+  #   ! 	  Logical NOT
+  #   & 	  Element-wise Logical AND
+  #   | 	  Element-wise Logical OR
+  #   && 	  Scalar Logical AND
+  #   || 	  Scalar Logical OR
+
+
+# MISCELLANEOUS OPERATORS
+  #   : 	  Creates a series of numbers in a sequence 	  x <- 1:10
+  #   %in% 	Find out if an element belongs to a vector 	  %in% y
+  #   %*% 	Matrix Multiplication 	                      x <- Matrix1 %*% Matrix2
