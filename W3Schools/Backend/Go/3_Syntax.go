c989e65ceec6ae22da4c051e0895aae4c7ad38b3
main <tompecher.official@gmail.com>
2025-02-14 23:21:46 +0000

    Go W3Schools Section 3

---
+++ W3Schools/Backend/Go/3_Syntax.go
@@ -0,0 +1,17 @@
+
+// Go: W3Schools - Main Tutorial
+// Section 3: Syntax
+
+// In Go, every program is part of a package: 
+package main
+
+// Import the "fmt" package, which contains functions for formatting text:
+import ("fmt")
+
+// The main function is the entry point of a Go program:
+func main() {
+	// Print "Hello World!" to the console using the Println function from the "fmt" package:
+	fmt.Println("Hello World!")
+}
+
+// NOTE: Go uses similar syntax to C++ with {} and ;, however Go does not require ; at the end of each statement.
