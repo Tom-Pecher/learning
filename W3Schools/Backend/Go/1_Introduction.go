f1ee7d7dbdee060403ec2eb558e1f2fb18b2f928
main <tompecher.official@gmail.com>
2025-02-14 23:00:17 +0000

    Go W3Schools Section 1

---
+++ W3Schools/Backend/Go/1_Introduction.go
@@ -0,0 +1,13 @@
+
+// Go: W3Schools - Main Tutorial
+// Section 1: Introduction
+
+// Go is a cross-platform, open source programming language that si used to create high-performance applications.
+// It is syntaxed similarly to C++ and behaves similarly, with a few notable exceptions.
+
+package main
+import ("fmt")
+
+func main() {
+	fmt.Println("Hello World!")
+}
