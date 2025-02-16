01379d8d28abaebecd54aca754c2a97785359fe2
main <tompecher.official@gmail.com>
2025-02-16 01:15:18 +0000

    DSA W3Schools Section 1

---
+++ W3Schools/Data_Analytics/DSA/Main_Tutorial/1_Introduction.py
@@ -0,0 +1,13 @@
+
+# DSA: W3Schools - Main Tutorial
+# Section 1: Introduction
+
+# KEY TERMS
+    # Algorithm 	        A set of step-by-step instructions to solve a specific problem.
+    # Data Structure 	    A way of organizing data so it can be used efficiently.
+    # Time Complexity 	    A measure of the amount of time an algorithm takes to run, depending on the amount of data the algorithm is working on.
+    # Space Complexity 	    A measure of the amount of memory an algorithm uses, depending on the amount of data the algorithm is working on.
+    # Big O Notation 	    A mathematical notation that describes the limiting behavior of a function when the argument tends towards a particular value or infinity.
+    # Recursion 	        A programming technique where a function calls itself.
+    # Divide and Conquer 	A method of solving complex problems by breaking them into smaller, more manageable sub-problems, solving the sub-problems, and combining the solutions.
+    # Brute Force 	        A simple and straight forward way an algorithm can work by simply trying all possible solutions and then choosing the best one.
