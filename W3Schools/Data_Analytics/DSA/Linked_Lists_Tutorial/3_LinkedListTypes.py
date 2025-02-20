ded9392846a2b444647473c73cb46d762a27cd54
main <tompecher.official@gmail.com>
2025-02-20 01:41:36 +0000

    DSA W3Schools LLs Section 3

---
+++ W3Schools/Data_Analytics/DSA/Linked_Lists_Tutorial/3_LinkedListTypes.py
@@ -0,0 +1,8 @@
+
+# DSA: W3Schools - Linked Lists Tutorial
+# Section 3: Linked List Types
+
+# There are three main "types" of linked lists:
+    # Singly linked lists:      each node has a reference to the next node
+    # Doubly linked lists:      each node has a reference to the next and previous node
+    # Circular linked lists:    each node has a reference to the next node, and the last node has a reference to the first node
