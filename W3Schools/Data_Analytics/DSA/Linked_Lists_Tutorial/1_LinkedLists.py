66857eb9e21fbb344c53f34ffe360d9c0d10ec20
main <tompecher.official@gmail.com>
2025-02-20 01:03:49 +0000

    DSA W3Schools LLs Section 1

---
+++ W3Schools/Data_Analytics/DSA/Linked_Lists_Tutorial/1_LinkedLists.py
@@ -0,0 +1,13 @@
+
+# DSA: W3Schools - Linked Lists Tutorial
+# Section 1: Linked Lists
+
+# Linked lists are a data structure consisting of nodes that are linked together to form a sequence.
+
+# Comparison of Arrays and Linked Lists:                                                                            ARRAYS 	LINKED LISTS
+    # An existing data structure in the programming language 	                                                     Yes 	 No
+    # Fixed size in memory 	                                                                                         Yes 	 No
+    # Elements, or nodes, are stored right after each other in memory (contiguously) 	                             Yes 	 No
+    # Memory usage is low (each node only contains data, no links to other nodes) 	                                 Yes 	 No
+    # Elements, or nodes, can be accessed directly (random access) 	                                                 Yes 	 No
+    # Elements, or nodes, can be inserted or deleted in constant time, no shifting operations in memory needed. 	 No      Yes
