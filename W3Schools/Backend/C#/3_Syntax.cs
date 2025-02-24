76defef9226b3b0132221e2c2f92478296dd0182
main <tompecher.official@gmail.com>
2025-02-24 23:40:53 +0000

    C# W3Schools Section 3

---
+++ W3Schools/Backend/C#/3_Syntax.cs
@@ -0,0 +1,20 @@
+
+// C#: W3Schools - Main Tutorial
+// Section 3: Syntax
+
+//// Using System means that we can use classes from the System namespace:
+// using System;
+
+//// A namespace is used to organize your code, and it is a container for classes and other namespaces:
+// namespace HelloWorld
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+
+//// Console is a class of the System namespace, which has a WriteLine() method that is used to output/print text:
+//       Console.WriteLine("Hello World!");    
+//     }
+//   }
+// }
