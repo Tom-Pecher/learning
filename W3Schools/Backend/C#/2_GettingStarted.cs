d26722d4d922b0a42ba0aeb334650fcbc9cd8692
main <tompecher.official@gmail.com>
2025-02-24 23:38:12 +0000

    C# W3Schools Section 2

---
+++ W3Schools/Backend/C#/2_GettingStarted.cs
@@ -0,0 +1,9 @@
+
+// C#: W3Schools - Main Tutorial
+// Section 2: Getting Started
+
+// In the W3Schools tutorial, you are encouraged to make a .NET Core project in Visual Studio.
+// Instead, I will be writing and running C# code as scripts.
+
+using System;
+Console.WriteLine("Hello World!");
