a649fed55d1a9e5358b0ccfa7a5e71eb69f3157b
main <tompecher.official@gmail.com>
2025-02-25 02:06:10 +0000

    C# W3Schools Section 7

---
+++ W3Schools/Backend/C#/7_DataTypes.cs
@@ -0,0 +1,30 @@
+
+// C#: W3Schools - Main Tutorial
+// Section 7: Data Types
+
+using System;
+
+// Information on data types in C#:
+    // int 	    4 bytes 	            Stores whole numbers from -2,147,483,648 to 2,147,483,647
+    // long 	8 bytes 	            Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+    // float 	4 bytes 	            Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+    // double 	8 bytes 	            Stores fractional numbers. Sufficient for storing 15 decimal digits
+    // bool 	1 byte 	                Stores true or false values
+    // char 	2 bytes 	            Stores a single character/letter, surrounded by single quotes
+    // string 	2 bytes per character 	Stores a sequence of characters, surrounded by double quotes
+
+// Integers are written normally:
+int myNum1 = 100000;
+
+// Long integers are written with an L:
+long myNum2 = 15000000000L;
+
+// Floats are written with an F:
+float myNum3 = 5.75F;
+
+// Doubles are written with a D:
+double myNum4 = 19.99D;
+
+// Scientific numbers are written with an E:
+float f1 = 35e3F;
+double d1 = 12E4D;
