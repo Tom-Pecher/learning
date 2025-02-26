cb8b9c98b4b70dfa9dcd60aff8b5a5d367020c8a
main <tompecher.official@gmail.com>
2025-02-26 12:47:45 +0000

    C# W3Schools Section 13

---
+++ W3Schools/Backend/C#/13_Booleans.cs
@@ -0,0 +1,15 @@
+
+// C#: W3Schools - Main Tutorial
+// Section 13: Booleans
+
+// Booleans are used for storing true/false values:
+bool isCSharpFun = true;
+bool isFishTasty = false;
+Console.WriteLine(isCSharpFun);
+Console.WriteLine(isFishTasty); 
+
+int myAge = 25;
+int votingAge = 18;
+
+// Comparison operators return booleans:
+Console.WriteLine(myAge >= votingAge)
