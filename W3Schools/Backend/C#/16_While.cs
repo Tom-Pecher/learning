cfccdfd6cc0866472a734efd914898d501476f8c
main <tompecher.official@gmail.com>
2025-02-26 12:55:25 +0000

    C# W3Schools Section 16

---
+++ W3Schools/Backend/C#/16_While.cs
@@ -0,0 +1,20 @@
+
+// C#: W3Schools - Main Tutorial
+// Section 16: While
+
+// A while loop excutes a block of code as long as its condition is met:
+int i = 0;
+while (i < 5) 
+{
+  Console.WriteLine(i);
+  i++;
+}
+
+// A do-while loop works the same, except it executes the code first before checking the condition:
+int j = 0;
+do 
+{
+  Console.WriteLine(j);
+  j++;
+}
+while (j < 5);
