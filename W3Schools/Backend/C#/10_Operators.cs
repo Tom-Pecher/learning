8d7ea2a396f5e5d8335ea81918246dac7a1aa14f
main <tompecher.official@gmail.com>
2025-02-26 12:22:48 +0000

    C# W3Schools Section 10

---
+++ W3Schools/Backend/C#/10_Operators.cs
@@ -0,0 +1,38 @@
+
+// C#: W3Schools - Main Tutorial
+// Section 10: Operators
+
+// ARITHMETIC OPERATORS
+    //  + 	    Addition 	    Adds together two values
+    //  - 	    Subtraction 	Subtracts one value from another
+    //  * 	    Multiplication 	Multiplies two values
+    //  / 	    Division 	    Divides one value by another
+    //  % 	    Modulus 	    Returns the division remainder	
+    //  ++ 	    Increment 	    Increases the value of a variable by 1	
+    //  -- 	    Decrement 	    Decreases the value of a variable by 1
+
+// COMPARISON OPERATORS
+    //  == 	    Equal to
+    //  != 	    Not equal
+    //  > 	    Greater than	
+    //  < 	    Less than	
+    //  >= 	    Greater than or equal to
+    //  <= 	    Less than or equal to
+
+// LOGICAL OPERATORS
+    //  &&  	Logical and
+    //  ||  	Logical or 	
+    //  ! 	    Logical not
+
+// ASSIGNMENT OPERATORS
+    //  = 	    x = 5	
+    //  += 	    x += 3
+    //  -= 	    x -= 3	
+    //  *= 	    x *= 3	
+    //  /= 	    x /= 3
+    //  %= 	    x %= 3	
+    //  &= 	    x &= 3
+    //  |= 	    x |= 3
+    //  ^= 	    x ^= 3
+    //  >>= 	x >>= 3
+    //  <<= 	x <<= 3
