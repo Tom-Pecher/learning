7ef9958d12cc00e413a52149126a0fe71a12fca1
main <tompecher.official@gmail.com>
2025-02-28 11:04:01 +0000

    Fixed naming issue

--- W3Schools/Backend/TypeScript/3_SimpleTypes/3_SimpleTypes.js
+++ W3Schools/Backend/TypeScript/3_SimpleTypes/3_SimpleTypes.js
@@ -1,5 +1,5 @@
 // TypeScript: W3Schools - Main Tutorial
-// Section 3: Syntax
+// Section 3: Simple Types
 // In TypeScript, we can assign a variable's type explicitly:
 var firstName1 = "Dylan";
 // Or impllicity:
--- W3Schools/Backend/TypeScript/3_SimpleTypes/3_SimpleTypes.ts
+++ W3Schools/Backend/TypeScript/3_SimpleTypes/3_SimpleTypes.ts
@@ -1,6 +1,6 @@
 
 // TypeScript: W3Schools - Main Tutorial
-// Section 3: Syntax
+// Section 3: Simple Types
 
 // In TypeScript, we can assign a variable's type explicitly:
 let firstName1: string = "Dylan";
