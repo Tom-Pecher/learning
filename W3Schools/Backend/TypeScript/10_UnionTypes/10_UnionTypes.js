ca9cdd0b1e888a2b41b2532645f7d978b6a95b4d
main <tompecher.official@gmail.com>
2025-03-01 08:52:57 +0000

    TS W3Schools Section 10

---
+++ W3Schools/Backend/TypeScript/10_UnionTypes/10_UnionTypes.js
@@ -0,0 +1,13 @@
+// TypeScript: W3Schools - Main Tutorial
+// Section 10: Union Types
+// Union types are used when a value can be more than a single type.
+// Using the | we are saying our parameter is a string or number:
+function printStatusCode1(code) {
+    console.log("My status code is ".concat(code, "."));
+}
+printStatusCode1(404);
+printStatusCode1('404');
+// NOTE: This can result in errors if the functionality does not support all of the types in the union:
+// function printStatusCode2(code: string | number) {
+//     console.log(`My status code is ${code.toUpperCase()}.`)
+// }
