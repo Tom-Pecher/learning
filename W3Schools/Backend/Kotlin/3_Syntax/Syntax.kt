f3bb912b49de87f917c02b8cdc658ac4abea52d4
main <tompecher.official@gmail.com>
2025-03-05 16:45:34 +0000

    Kotlin W3Schools Section 3

---
+++ W3Schools/Backend/Kotlin/3_Syntax/Syntax.kt
@@ -0,0 +1,13 @@
+
+// KOTLIN: W3Schools - Main Tutorial
+// Section 3: Syntax
+
+// Any code inside the main() function's curly brackets {} will be executed:
+fun main() {
+
+    // The println() function is used to output/print text to the console:
+    println("Hello, Kotlin!")
+
+}
+
+// NOTE: Kotlin does not require a semicolon at the end of each statement, like other similarly syntaxed languages.
