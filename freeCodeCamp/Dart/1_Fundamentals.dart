84b0699424579779aa613bd0b045b485dec35106
main <tompecher.official@gmail.com>
2025-03-10 22:08:58 +0000

    Dart freeCodeCamp Section 1

---
+++ freeCodeCamp/Dart/1_Fundamentals.dart
@@ -0,0 +1,15 @@
+
+// DART: freeCodeCamp - https://www.youtube.com/watch?v=Ej_Pcr4uC2Q
+// Section 1: Variables
+
+main() {
+  // Variables:
+    // Dart supports dynamic typing, with the 'var' keyword:
+    var name = 'John';
+
+    // Dart also supports static typing:
+    String name2 = 'Doe';
+
+    // Print to console:
+    print(name + ' ' + name2);
+}
