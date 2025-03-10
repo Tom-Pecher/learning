ffb58af99b41051ee565f853edbeda205e672845
main <tompecher.official@gmail.com>
2025-03-10 22:13:46 +0000

    Dart freeCodeCamp Section 3

---
+++ freeCodeCamp/Dart/3_Comments.dart
@@ -0,0 +1,13 @@
+
+// DART: freeCodeCamp - https://www.youtube.com/watch?v=Ej_Pcr4uC2Q
+// Section 3: Comments
+
+// This is a single line comment.
+
+/*
+This is a 
+multi-line comment.
+*/
+
+main() {
+}
