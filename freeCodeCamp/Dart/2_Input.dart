67ab704f037b379e9ccfccb4ee89cfdb4823b82d
main <tompecher.official@gmail.com>
2025-03-10 22:12:40 +0000

    Dart freeCodeCamp Section 2

---
+++ freeCodeCamp/Dart/2_Input.dart
@@ -0,0 +1,13 @@
+
+// DART: freeCodeCamp - https://www.youtube.com/watch?v=Ej_Pcr4uC2Q
+// Section 2: Input
+
+// Import IO functionality:
+import 'dart:io';
+
+main() {
+  // Take user input from the console:
+    stdout.writeln('What is your name?');
+    var name3 = stdin.readLineSync();
+    print('My name is $name3');
+}
