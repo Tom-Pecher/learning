b4bf15c9bb95fdeda596e01eac7074995ef62ec3
main <tompecher.official@gmail.com>
2025-03-11 17:46:53 +0000

    Dart freeCodeCamp Section 7

---
+++ freeCodeCamp/Dart/7_Operators.dart
@@ -0,0 +1,56 @@
+
+// DART: freeCodeCamp - https://www.youtube.com/watch?v=Ej_Pcr4uC2Q
+// Section 7: Operators
+
+main() {
+}
+
+// Arithmetic Operators
+  //  +     Addition
+  //  -     Subtraction
+  //  *     Multiplication
+  //  /     Division
+  //  ~/    Integer Division
+  //  %     Modulus
+  //  ++    Increment
+  //  --    Decrement
+
+// Assignment Operators
+  //  =     Assign
+  //  +=    Add and Assign
+  //  -=    Subtract and Assign
+  //  *=    Multiply and Assign
+  //  /=    Divide and Assign
+  //  ~/=   Integer Division and Assign
+  //  %=    Modulus and Assign
+
+// Relational Operators
+  //  ==    Equal
+  //  !=    Not Equal
+  //  >     Greater Than
+  //  <     Less Than
+  //  >=    Greater Than or Equal
+  //  <=    Less Than or Equal
+
+// Logical Operators
+  //  &&    And
+  //  ||    Or
+  //  !     Not
+
+// Type Test Operators
+  //  as    Typecast
+  //  is    True if the object has the specified type
+  //  is!   False if the object has the specified type
+
+// Bitwise Operators
+  //  &     AND
+  //  |     OR
+  //  ^     XOR
+  //  ~     NOT
+  //  <<    Shift Left
+  //  >>    Shift Right
+
+// Null Aware Operators
+  //  ?.    Execute a method if the object is not null
+  //  ??    Assign a default value if the variable is null
+  //  ??=   Assign a value if the variable is null
