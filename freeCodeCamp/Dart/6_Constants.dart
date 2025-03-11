0a6d8d4f9dd6e1ebdb8d7ea855ca51e62df3eaef
main <tompecher.official@gmail.com>
2025-03-11 17:34:57 +0000

    Dart freeCodeCamp Section 6

---
+++ freeCodeCamp/Dart/6_Constants.dart
@@ -0,0 +1,16 @@
+
+// DART: freeCodeCamp - https://www.youtube.com/watch?v=Ej_Pcr4uC2Q
+// Section 6: Constants
+
+main() {
+  // Constants are variables whose values cannot be changed once they are assigned:
+  const int x = 5;
+  print(x);
+
+  // Variables which are declared but not assigned are given the null value:
+  int y;
+  y = 10;
+  print(y);
+  
+  // NOTE: Constants must be assigned a value when they are declared.
+}
