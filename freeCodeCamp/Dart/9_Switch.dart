38be405e51f68812f981d0f2ea882f1556fa6211
main <tompecher.official@gmail.com>
2025-03-11 17:52:51 +0000

    Dart freeCodeCamp Section 9

---
+++ freeCodeCamp/Dart/9_Switch.dart
@@ -0,0 +1,24 @@
+
+// DART: freeCodeCamp - https://www.youtube.com/watch?v=Ej_Pcr4uC2Q
+// Section 9: Switch
+
+main() {
+  // Switch Case:
+  var grade = "A";
+  switch (grade) {
+    case "A":
+      print("Excellent");
+      break;
+    case "B":
+      print("Good");
+      break;
+    case "C":
+      print("Average");
+      break;
+    case "D":
+      print("Poor");
+      break;
+    default:
+      print("Invalid grade");
+  }
+}
