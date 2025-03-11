13d42c1e1d531054635bba1c347dbdd3fef73c43
main <tompecher.official@gmail.com>
2025-03-11 17:30:44 +0000

    Finished Dart Section 5

--- freeCodeCamp/Dart/5_Strings.dart
+++ freeCodeCamp/Dart/5_Strings.dart
@@ -16,4 +16,20 @@ main() {
   // However, if we do not with to evaluate escape characters, we can create a raw string with r:
   String rawString = r'Hello, \nWorld!';
   print(rawString);
+
+  // We can parse strings to integers and doubles:
+  String a = '30';
+  String b = '3.14';
+  int aInt = int.parse(a);
+  double bDouble = double.parse(b);
+  print(aInt);
+  print(bDouble);
+
+  // Convert integers and doubles to strings:
+  int c = 30;
+  double d = 3.14159;
+  String cString = c.toString();
+  String dString = d.toStringAsFixed(3); // 3 decimal places
+  print(cString);
+  print(dString);
 }
