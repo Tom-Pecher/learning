e6e106a5f1088bb1dc8f9de73182093065a121e2
main <tompecher.official@gmail.com>
2025-03-11 17:49:53 +0000

    Dart freeCodeCamp Section 8

---
+++ freeCodeCamp/Dart/8_ConditionalStatements.dart
@@ -0,0 +1,17 @@
+
+// DART: freeCodeCamp - https://www.youtube.com/watch?v=Ej_Pcr4uC2Q
+// Section 8: Conditional Statements
+
+main() {
+  // If-Else Statement:
+  var marks = 70;
+  if (marks >= 90 && marks < 100) {
+    print("A+ grade");
+  } else if (marks >= 80 && marks < 90) {
+    print("A grade");
+  } else if (marks >= 70 && marks < 80) {
+    print("B grade");
+  } else {
+    print("Invalid marks");
+  }
+}
