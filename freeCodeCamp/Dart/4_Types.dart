a42cabacf03e9f2fe4c6408cd0b0325fdf6923ab
main <tompecher.official@gmail.com>
2025-03-11 17:25:30 +0000

    Fixed Section 4 Unused Variables

--- freeCodeCamp/Dart/4_Types.dart
+++ freeCodeCamp/Dart/4_Types.dart
@@ -11,9 +11,16 @@ main() {
   bool isTrue = true;
 
   // To infer a variable's type at compile time, we can use the keyword 'var':
-  var variable = 'This is a dynamic variable';
+  var variable = 'This is a variable';
 
   // In a dynamically typed language, a variable's type is known at runtime (its type is inferred).
   // For this, we can use the keyword 'dynamic':
   dynamic dynamicVar = 'This is a dynamic variable';
+
+  print(age);
+  print(pi);
+  print(name);
+  print(isTrue);
+  print(variable);
+  print(dynamicVar);
 }
