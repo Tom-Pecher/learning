b23f3ff95f4313820b9dd982e18fe3eb3facada7
main <tompecher.official@gmail.com>
2025-03-15 22:11:22 +0000

    MATLAB OnRamp Section D

---
+++ Official_Tutorials/MATLAB/D_Commands.m
@@ -0,0 +1,19 @@
+
+% MATLAB: MathWorks, OnRamp
+% Section 3: Commands
+
+% Use 'save' to save variables to a file:
+x = 1;
+save('x.mat', 'x');
+
+% Use clear to remove variables from the workspace (or specific ones):
+clear x;
+
+% Load variables from a file using 'load':
+load('x.mat');
+
+% See the contents of a variable by calling its name:
+y = 2;
+y;
+
+% Use 'clc' to clear the command window.
