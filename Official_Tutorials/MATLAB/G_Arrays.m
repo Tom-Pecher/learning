7e092726f9fbb4f53b42e0033802366880ed0be4
main <tompecher.official@gmail.com>
2025-03-15 22:26:50 +0000

    MATLAB OnRamp Section G

---
+++ Official_Tutorials/MATLAB/G_Arrays.m
@@ -0,0 +1,20 @@
+
+% MATLAB: MathWorks, OnRamp
+% Section 6: Arrays
+
+% Single values are called scalars:
+x = 5;
+
+% Vectors are 1D arrays.
+% Row vector (separated by spaces):
+v1 = [1 2 3 4 5];
+
+% Column vector (separated by semicolons):
+v2 = [1; 2; 3; 4; 5];
+
+% Matrices are 2D arrays:
+A = [1 2 3; 4 5 6; 7 8 9];
+
+% We can still perform operations when initializing arrays:
+v3 = [1+2 2*5 3^2];
+disp(v3);
