07ad608a693d9ec06ed232f617b4ae7f06c9ee41
main <tompecher.official@gmail.com>
2025-03-15 22:02:03 +0000

    MATLAB OnRamp Section C

---
+++ Official_Tutorials/MATLAB/C_Variables.m
@@ -0,0 +1,8 @@
+
+% MATLAB: MathWorks, OnRamp
+% Section 2: Variables
+
+% Variables can be given any name, so long as:
+% - They start with a letter
+% - They contain only letters, numbers, and underscores
+% - They are not a reserved word (e.g. 'if', 'end', 'for')
