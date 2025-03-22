11321906a86ce048b9d56496aae816e92e7bede1
main <tompecher.official@gmail.com>
2025-03-22 14:13:11 +0000

    MATLAB OnRamp Section 8

---
+++ Official_Tutorials/MATLAB/I_Generate.m
@@ -0,0 +1,15 @@
+
+% MATLAB: MathWorks, OnRamp
+% Section 8: Generate
+
+% Create a matrix of random numbers with specified dimensions:
+x = rand(2, 3)
+disp(x)
+
+% Create a matrix of zeros with specified dimensions:
+y = zeros(2, 3)
+disp(y)
+
+% Create a matrix of ones with specified dimensions:
+z = ones(2, 3)
+disp(z)
