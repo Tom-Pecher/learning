a3d4638bf4f38ee40bcb39f57c93eb6bbc79021b
main <tompecher.official@gmail.com>
2025-03-22 14:41:57 +0000

    MATLAB OnRamp Section 13

--- Official_Tutorials/MATLAB/N_Documentation.m
+++ Official_Tutorials/MATLAB/N_Documentation.m
@@ -3,3 +3,7 @@
 % Section 13: Documentation
 
 % Use the MATLAB documentation to learn more about the functions you are using.
+doc randi
+
+% If you do not know the exact name, search with a phrase:
+doc 'normally distributed numbers'
