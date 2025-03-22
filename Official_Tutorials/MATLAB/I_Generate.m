8c97d5bcafc999c10f0a27e601f11df36ab6e4f1
main <tompecher.official@gmail.com>
2025-03-22 14:15:44 +0000

    MATLAB OnRamp Section 8

--- Official_Tutorials/MATLAB/I_Generate.m
+++ Official_Tutorials/MATLAB/I_Generate.m
@@ -4,12 +4,13 @@
 
 % Create a matrix of random numbers with specified dimensions:
 x = rand(2, 3)
-disp(x)
 
 % Create a matrix of zeros with specified dimensions:
 y = zeros(2, 3)
-disp(y)
 
 % Create a matrix of ones with specified dimensions:
 z = ones(2, 3)
-disp(z)
+
+% One can determine the size of an existing matrix:
+
+s = size(x)
