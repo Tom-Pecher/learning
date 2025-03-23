dcbce1d37a837c7426bd167df221ed94c238b459
main <tompecher.official@gmail.com>
2025-03-23 23:23:04 +0000

    Dart freeCodeCamp Section 11

---
+++ freeCodeCamp/Dart/11_WhileLoops.dart
@@ -0,0 +1,19 @@
+
+// DART: freeCodeCamp - https://www.youtube.com/watch?v=Ej_Pcr4uC2Q
+// Section 11: While Loops
+
+main() {
+  // While loop:
+  var i = 0;
+  while (i < 5) {
+    print(i);
+    i++;
+  }
+
+  // Do-while loop:
+  var j = 0;
+  do {
+    print(j);
+    j++;
+  } while (j < 5);
+}
