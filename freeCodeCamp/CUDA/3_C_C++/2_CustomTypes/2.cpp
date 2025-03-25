2952d92600e47119a077692e5bbdd902a2f777e1
main <tompecher.official@gmail.com>
2025-03-25 00:42:10 +0000

    CUDA freeCodeCamp C/C++ Mini Section 2.2 (C++)

---
+++ freeCodeCamp/CUDA/3_C_C++/2_CustomTypes/2.cpp
@@ -0,0 +1,17 @@
+
+// C++ Mini Tutorial
+// Section 2: Custom Types
+
+#include <iostream>
+
+using namespace std;
+
+typedef struct {
+    float x;
+    float y;
+} Point;
+
+int main() {
+    Point p1 = {1.5, 2.5};
+    cout << "Size of Point: " << sizeof(Point) << endl;
+}
