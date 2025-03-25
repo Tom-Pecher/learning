5dd77684c066456bbd4776c35761b336965a7b62
main <tompecher.official@gmail.com>
2025-03-25 00:34:48 +0000

    CUDA freeCodeCamp C/C++ Mini Section 2.1

---
+++ freeCodeCamp/CUDA/3_C_C++/2_CustomTypes/1.c
@@ -0,0 +1,15 @@
+
+// C Mini Tutorial
+// Section 1: Size_T
+
+#include <stdio.h>
+
+int main() {
+    int arr1[] = {1, 2, 3, 4, 5};
+
+    // The size_t data type is used to store the size of a variable in bytes:
+    size_t size = sizeof(arr1) / sizeof(arr1[0]);
+    printf("Size of array: %zu\n", size);
+    printf("Size of size_t: %zu\n", sizeof(size_t)); // Long unsigned integer
+    printf("Size of int: %zu\n", sizeof(int));
+}
