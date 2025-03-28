c139715066bc837a18067cfc52076b8014e76ae7
main <tompecher.official@gmail.com>
2025-03-28 02:43:17 +0000

    SQL W3Schools Section 2

---
+++ W3Schools/Backend/SQL/2_Syntax/queries.sql
@@ -0,0 +1,22 @@
+
+-- SQL: W3Schools - Main Tutorial
+-- Section 2: Syntax
+
+-- Generate SELECT queries for each table
+SELECT * FROM Customers;
+
+-- SQL keywords are not case sensitive:
+select * from customers;
+
+-- Important SQL commands:
+    -- SELECT           - extracts data from a database
+    -- UPDATE           - updates data in a database
+    -- DELETE           - deletes data from a database
+    -- INSERT INTO      - inserts new data into a database
+    -- CREATE DATABASE  - creates a new database
+    -- ALTER DATABASE   - modifies a database
+    -- CREATE TABLE     - creates a new table
+    -- ALTER TABLE      - modifies a table
+    -- DROP TABLE       - deletes a table
+    -- CREATE INDEX     - creates an index (search key)
+    -- DROP INDEX       - deletes an index
