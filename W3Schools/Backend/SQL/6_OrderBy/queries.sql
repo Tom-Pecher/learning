ad33014b1ee647ab96c11d0d2bc62598b53c862c
main <tompecher.official@gmail.com>
2025-03-28 04:08:56 +0000

    SQL W3Schools Section 6

---
+++ W3Schools/Backend/SQL/6_OrderBy/queries.sql
@@ -0,0 +1,15 @@
+
+-- SQL: W3Schools - Main Tutorial
+-- Section 5: Where
+
+-- The order by keyword is used to sort the result-set in ascending or descending order:
+SELECT * FROM products
+ORDER BY Price;
+
+-- SELECT column1, column2, ...
+-- FROM table_name
+-- ORDER BY column1, column2, ... ASC|DESC;
+
+-- We can also order by multiple columns (first order by column1, then order by column2):
+SELECT * FROM Customers
+ORDER BY Country ASC, CustomerName DESC;
