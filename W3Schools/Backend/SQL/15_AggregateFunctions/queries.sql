5994dbe746c8f5b3d2228a7a0d5189f34d00b0e4
main <tompecher.official@gmail.com>
2025-03-29 08:51:35 +0000

    SQL W3Schools Section 15

---
+++ W3Schools/Backend/SQL/15_AggregateFunctions/queries.sql
@@ -0,0 +1,13 @@
+
+-- SQL: W3Schools - Main Tutorial
+-- Section 15: Aggregate Functions
+
+-- An aggregate function is a function that performs a calculation on a set of values, and returns a single value.
+-- The main ones include:
+    -- MIN()    - returns the smallest value within the selected column
+    -- MAX()    - returns the largest value within the selected column
+    -- COUNT()  - returns the number of rows in a set
+    -- SUM()    - returns the total sum of a numerical column
+    -- AVG()    - returns the average value of a numerical column
+
+-- NOTE: Aggregate functions ignore null values (except for COUNT).
