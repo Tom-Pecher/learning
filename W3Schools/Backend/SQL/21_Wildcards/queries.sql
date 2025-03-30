e9a749b66169c3650d4dbf0196d0b4387aa722ab
main <tompecher.official@gmail.com>
2025-03-30 12:02:25 +0100

    SQL W3Schools Section 21

---
+++ W3Schools/Backend/SQL/21_Wildcards/queries.sql
@@ -0,0 +1,20 @@
+
+-- SQL: W3Schools - Main Tutorial
+-- Section 21: Wildcards
+
+-- The like keyword is used for pattern matching:
+    --  % 	Represents zero or more characters
+    --  _ 	Represents a single character
+    --  [] 	Represents any single character within the brackets (not in PostgreSQL or MySQL)
+    --  ^ 	Represents any character not in the brackets (not in PostgreSQL or MySQL)
+    --  - 	Represents any single character within the specified range (not in PostgreSQL or MySQL)
+    --  {} 	Represents any escaped character (only in Oracle)
+
+-- Microsoft Access Databases:
+    --  * 	Represents zero or more characters
+    --  ? 	Represents a single character
+    --  [] 	Represents any single character within the brackets
+    --  ! 	Represents any character not in the brackets
+    --  - 	Represents any single character within the specified range
+    --  # 	Represents any single numeric character
+    
