a86b86f52ce3f0882630e98e86ab940e08dec24f
main <tompecher.official@gmail.com>
2025-03-30 12:54:35 +0100

    SQL W3Schools Section 24

---
+++ W3Schools/Backend/SQL/24_Joins/queries.sql
@@ -0,0 +1,9 @@
+
+-- SQL: W3Schools - Main Tutorial
+-- Section 24: Joins
+
+-- Joins are used to combine the contents of multiple tables:
+    -- (INNER) JOIN:        Returns records that have matching values in both tables
+    -- LEFT (OUTER) JOIN:   Returns all records from the left table, and the matched records from the right table
+    -- RIGHT (OUTER) JOIN:  Returns all records from the right table, and the matched records from the left table
+    -- FULL (OUTER) JOIN:   Returns all records when there is a match in either left or right table
