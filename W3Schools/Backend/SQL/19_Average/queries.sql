286c4ad458fb14b7a720685b4f90bda83a6aa8cd
main <tompecher.official@gmail.com>
2025-03-30 11:25:28 +0100

    SQL W3Schools Section 19

---
+++ W3Schools/Backend/SQL/19_Average/queries.sql
@@ -0,0 +1,11 @@
+
+-- SQL: W3Schools - Main Tutorial
+-- Section 19: Average
+
+-- The avg function is used to calculate the average value of a set of values:
+SELECT AVG(Price)
+FROM Products;
+
+-- SELECT AVG(column_name)
+-- FROM table_name
+-- WHERE condition; 
