2988117f52d3944b49881e0aa22879dfaa6d0b0d
main <tompecher.official@gmail.com>
2025-04-03 16:06:55 +0100

    Rust RBEx Section 1

---
+++ Official_Tutorials/Rust/Rust_By_Example/1_HelloWorld/1_HelloWorld.rs
@@ -0,0 +1,16 @@
+
+// RUST: Rust By Example
+// Section 1: Hello World
+
+// This is a comment, and is ignored by the compiler.
+
+// This is the main function.
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+
+    // Print text to the console.
+    println!("Hello World!");
+
+    // Activity 1:
+    println!("I'm a Rustacean!");
+}
