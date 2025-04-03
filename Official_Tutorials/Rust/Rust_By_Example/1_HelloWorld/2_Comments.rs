e872a83ea678a12f06a16741f8d7a8e30fe10d54
main <tompecher.official@gmail.com>
2025-04-03 16:11:06 +0100

    Rust RBEx Section 1.2

---
+++ Official_Tutorials/Rust/Rust_By_Example/1_HelloWorld/2_Comments.rs
@@ -0,0 +1,13 @@
+
+// RUST: Rust By Example
+// Section 1.2: Comments
+
+// This is a single-line comment.
+/* This is a 
+multi-line comment. */
+
+fn main() {
+    // We can edit lines of code more easily with block comments:
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
