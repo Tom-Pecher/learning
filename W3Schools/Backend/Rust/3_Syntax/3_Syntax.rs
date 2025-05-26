39d8ffa87814f5d853c07abc4803cd3a2bce5f94
main <tompecher.official@gmail.com>
2025-05-26 17:16:50 +0100

    Rust W3Schools Section 3

---
+++ W3Schools/Backend/Rust/3_Syntax/3_Syntax.rs
@@ -0,0 +1,10 @@
+
+// Rust: W3Schools - Main Tutorial
+// Section 3: Syntax:
+
+// The main function is the entry point of a Rust program:
+fn main() {
+
+    // The println! macro prints a message to the console:
+    println!("Hello, world!");
+}
