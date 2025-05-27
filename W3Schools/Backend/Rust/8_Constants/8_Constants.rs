28dcb251f73724063a8df9ee8d6186f29c109ac3
main <tompecher.official@gmail.com>
2025-05-27 21:25:33 +0100

    Rust W3Schools Section 8

---
+++ W3Schools/Backend/Rust/8_Constants/8_Constants.rs
@@ -0,0 +1,10 @@
+
+// Rust: W3Schools - Main Tutorial
+// Section 8: Constants
+
+fn main() {
+    // Constants are like variables except that once they are defined, they cannot be changed or redefined.
+    // They must be assigned a value and type when they are declared:
+    const _BIRTHYEAR: i32 = 1980;
+    const _MINUTES_PER_HOUR: i32 = 60;
+}
