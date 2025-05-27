14be777bb87187cbb5644fc74151783a4a90950a
main <tompecher.official@gmail.com>
2025-05-27 21:29:06 +0100

    Rust W3Schools Section 9

---
+++ W3Schools/Backend/Rust/9_Operators/9_Operators.rs
@@ -0,0 +1,35 @@
+
+// Rust: W3Schools - Main Tutorial
+// Section 9: Operators
+
+fn main() {}
+
+// ARITHMETIC OPERATORS
+    //   + 	    
+    //   - 	    Subtraction
+    //   * 	    Multiplication
+    //   / 	    Division
+    //   ^ 	    Exponent
+    //   %	    Modulus
+
+
+// ASSIGNMENT OPERATORS
+    //   = 	    Assignment
+    //   += 	Add and assign
+    //   -= 	Subtract and assign
+    //   *= 	Multiply and assign
+    //   /= 	Divide and assign
+    //   %= 	Modulus and assign
+
+// COMPARISON OPERATORS
+    //   == 	Equal to
+    //   != 	Not equal to
+    //   > 	    Greater than
+    //   < 	    Less than
+    //   >= 	Greater than or equal to
+    //   <= 	Less than or equal to
+
+// LOGICAL OPERATORS
+    //   && 	Logical and
+    //   || 	Logical or
+    //   ! 	    Logical not
