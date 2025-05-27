c4f5e75b7f24fd2ecbd0359efc6b32c35ada1008
main <tompecher.official@gmail.com>
2025-05-27 21:20:51 +0100

    Rust W3Schools Section 7

---
+++ W3Schools/Backend/Rust/7_DataTypes/7_DataTypes.rs
@@ -0,0 +1,15 @@
+
+// Rust: W3Schools - Main Tutorial
+// Section 7: Data Types
+
+fn main() {
+    // In Rust, the variable type can be infered from the value assigned to it:
+    let _x = 5; // x is of type i32
+
+    // However, it is better to define the variable type explicitly:
+    let _my_num: i32 = 5;          // integer
+    let _my_double: f64 = 5.99;    // float
+    let _my_letter: char = 'D';    // character
+    let _my_bool: bool = true;     // boolean
+    let _my_text: &str = "Hello";  // string
+}
