f6a2709901741ca638bb35daeaef9a58406f2999
main <tompecher.official@gmail.com>
2025-05-27 21:04:02 +0100

    Rust W3Schools Section 4

---
+++ W3Schools/Backend/Rust/4_Output/4_Output.rs
@@ -0,0 +1,16 @@
+
+// Rust: W3Schools - Main Tutorial
+// Section 4: Output:
+
+fn main() {
+
+    // Print a line of text (creates a new line):
+    println!("Hello World!");
+
+    // Print a line of text (on the same line):
+    print!("Hello World! ");
+    print!("I will print on the same line.");
+
+    // We can manually create a new line using an escape character:
+    println!("Hello World!\nI will print on a new line.");
+}
