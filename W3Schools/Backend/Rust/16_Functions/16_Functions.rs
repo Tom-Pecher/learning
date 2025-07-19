5d185e07bf325b765bd91b1dde51faed07316eb1
main <tompecher.official@gmail.com>
2025-07-19 23:35:12 +0100

    Rust W3Schools Section 16

---
+++ W3Schools/Backend/Rust/16_Functions/16_Functions.rs
@@ -0,0 +1,17 @@
+
+// Rust: W3Schools - Main Tutorial
+// Section 16: Functions
+
+fn main() {
+    
+    // Functions in Rust are defined using the fn keyword:
+    fn add(a: i32, b: i32) -> i32 {
+        let c = a + b;
+
+        // Whilst Rust has a return keyword, we can simply return the value directly (without semicolon):
+        c
+    }
+    
+    let sum = add(3, 4);
+    println!("Sum is: {}", sum);
+}
